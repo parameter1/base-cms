b8957f3e339153bd2fe1be38638a30e3a5ba76ff
Jacob Bare <jacob@parameter1.com>
2021-01-12 12:04:16 -0500

    Remove caprica from hooks service

--- services/hooks/src/env.js
+++ services/hooks/src/env.js
@@ -11,7 +11,6 @@ module.exports = cleanEnv(process.env, {
   ENABLE_BASEDB_LOGGING: bool({ desc: 'Whether the BaseDB instance should log to the console.', default: false }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
-  CAPRICA_DSN: nonemptystr({ desc: 'The Base Caprica MongoDB connection URL.' }),
   LEONIS_DSN: nonemptystr({ desc: 'The Base Leonis MongoDB connection URL.' }),
   TAURON_DSN: nonemptystr({ desc: 'The Base Tauron MongoDB connection URL.' }),
   VIRGON_DSN: nonemptystr({ desc: 'The Base Tauron MongoDB connection URL.' }),
