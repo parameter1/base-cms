d15d96bf4e58d37cc073863bef6c1e9638875c27
Josh Worden <josh@parameter1.com>
2021-01-13 16:20:20 -0600

    Revert "Remove caprica from hooks service"
    This reverts commit b8957f3e339153bd2fe1be38638a30e3a5ba76ff.

--- services/hooks/src/basedb/index.js
+++ services/hooks/src/basedb/index.js
@@ -1,8 +1,10 @@
+const caprica = require('./caprica');
 const leonis = require('./leonis');
 const tauron = require('./tauron');
 const virgon = require('./virgon');
 
 module.exports = tenant => ({
+  caprica: caprica(tenant),
   leonis: leonis(tenant),
   tauron: tauron(tenant),
   virgon: virgon(tenant),
