49550552c1b582c5fa3ce53bb28e37bea69c4a23
Jacob Bare <jacob@parameter1.com>
2021-01-26 23:06:19 -0600

    Export EJSON from root of db package

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -1,9 +1,11 @@
+const EJSON = require('mongodb-extended-json');
 const BaseDB = require('./basedb');
 const MongoDB = require('./mongodb');
 const createMongoClient = require('./create-mongo-client');
 const createBaseDB = require('./create-basedb');
 
 module.exports = {
+  EJSON,
   BaseDB,
   MongoDB,
   createMongoClient,
