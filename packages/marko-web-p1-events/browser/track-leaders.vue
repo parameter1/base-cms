8ad158f845ea59f61497c09def774f4b43d65456
Jacob Bare <jacob@parameter1.com>
2021-03-02 08:55:43 -0600

    Create leaders tracking browser component

---
+++ packages/marko-web-p1-events/browser/track-leaders.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="p1-events-track-leaders" />
+</template>
+
+<script>
+export default {
+  inject: ['EventBus'],
+  props: {
+    baseTenantKey: {
+      type: String,
+      required: true,
+    },
+    eventName: {
+      type: String,
+      default: 'leaders-action',
+    },
+  },
+
+  created() {
+    this.EventBus.$on(this.eventName, (event, payload) => {
+      const { p1events } = window;
+      if (!p1events) return;
+      const { type, category, label } = event;
+
+      const queue = [];
+      const baseEvent = {
+        action: type,
+        category,
+        label,
+      };
+      if (type === 'viewed' && category === 'Leaders Sections Nav' && label === 'Section Company Items') {
+        payload.items.forEach((item) => {
+          queue.push({
+            ...baseEvent,
+            action: 'View',
+            entity: { id: item.id, ns: this.ns('content-company') },
+            context: { id: payload.sectionId, ns: this.ns('website-section') },
+          });
+        });
+      } else if (type === 'expand' && category === 'Leaders Sections Nav' && label === 'Section Item') {
+        queue.push({
+          ...baseEvent,
+          action: 'Expand',
+          entity: { id: payload.sectionId, ns: this.ns('website-section') },
+        });
+      } else if (type === 'open' && category === 'Leaders Data Card') {
+        queue.push({
+          ...baseEvent,
+          action: 'Open',
+          entity: { id: payload.companyId, ns: this.ns('content-company') },
+        });
+      } else if (type === 'click' && category === 'Leaders Data Card') {
+        queue.push({
+          ...baseEvent,
+          action: 'Click',
+          entity: { id: payload.companyId, ns: this.ns('content-company') },
+          props: { url: payload.href },
+        });
+      }
+      if (queue.length) p1events('track', queue);
+    });
+  },
+
+  methods: {
+    matches(event, type, category, label) {
+      return event.category === category
+        && event.label === label
+        && event.type === type;
+    },
+    ns(type) {
+      return `base.${this.baseTenantKey.replace('_', '-')}.${type}`;
+    },
+  },
+};
+</script>
