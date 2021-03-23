186d1ba8925186e9be864dd9b5aaadb84207d2b3
Jacob Bare <jacob@parameter1.com>
2021-03-23 09:39:44 -0500

    Add support for outputting pretty HTML

--- packages/marko-core/middleware/clean-marko-response.js
+++ packages/marko-core/middleware/clean-marko-response.js
@@ -1,10 +1,37 @@
+const { getAsObject } = require('@parameter1/base-cms-object-path');
+const pretty = require('pretty');
 const cleanChunk = require('../utils/clean-marko-chunk');
+const buildMarkoGlobal = require('../utils/build-marko-global');
 
-module.exports = ({ enabled = true } = {}) => (req, res, next) => {
+module.exports = ({ enabled = true, prettyQueryParam = 'pretty', prettyEnvVar = 'MARKO_PRETTY_OUTPUT' } = {}) => (req, res, next) => {
+  if (!enabled || !res.marko) return next();
   const { write } = res;
+  const prettyOutput = process.env[prettyEnvVar]
+    || Object.hasOwnProperty.call(req.query, prettyQueryParam);
+
+  if (prettyOutput) {
+    res.marko = function output(template, data) {
+      if (typeof template === 'string') {
+        throw new Error('The Marko template cannot be a string.');
+      }
+      const $global = buildMarkoGlobal(this);
+      const d = { ...data, $global: { ...$global, ...getAsObject(data, '$global') } };
+      this.set({ 'content-type': 'text/html; charset=utf-8' });
+      const out = template.createOut();
+      template.render(d, out);
+
+      out.on('finish', () => {
+        const html = cleanChunk(out.getOutput());
+        this.send(pretty(html));
+      });
+      out.end();
+    };
+    return next();
+  }
+
   res.write = function clean(...args) {
     const [chunk] = args;
-    if (!enabled || typeof chunk !== 'string') {
+    if (typeof chunk !== 'string') {
       write.apply(this, args);
     } else {
       const cleanedArgs = [...args];
@@ -12,5 +39,5 @@ module.exports = ({ enabled = true } = {}) => (req, res, next) => {
       write.apply(this, cleanedArgs);
     }
   };
-  next();
+  return next();
 };
