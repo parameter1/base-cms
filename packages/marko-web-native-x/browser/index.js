e856ece6ac3169d39dc50f655bb900da4c47f54f
Josh Worden <josh@parameter1.com>
2021-03-31 16:33:01 -0500

    Remove browser components, use gtm-push

--- packages/marko-web-native-x/browser/index.js
+++
@@ -1,5 +0,0 @@
-const TrackNativeXStoryPageView = () => import(/* webpackChunkName: "native-x-story-track-page-view" */ './track-page-view.vue');
-
-export default (Browser) => {
-  Browser.register('TrackNativeXStoryPageView', TrackNativeXStoryPageView);
-};
