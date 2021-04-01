54db8fe89cffad315b15c2151aaeec4e03d7323b
Josh Worden <josh@parameter1.com>
2021-04-01 12:36:52 -0500

    Add social share component

--- packages/marko-web-native-x/browser/index.js
+++ packages/marko-web-native-x/browser/index.js
@@ -1,5 +1,8 @@
 const NativeXTrackEndOfContent = () => import(/* webpackChunkName: "native-x-track-end-of-content" */ './track-end-of-content.vue');
+const NativeXTrackSocialShare = () => import(/* webpackChunkName: "native-x-track-social-share" */ './track-social-share.vue');
 
 export default (Browser) => {
+  const { EventBus } = Browser;
   Browser.register('NativeXTrackEndOfContent', NativeXTrackEndOfContent);
+  Browser.register('NativeXTrackSocialShare', NativeXTrackSocialShare, { provide: { EventBus } });
 };
