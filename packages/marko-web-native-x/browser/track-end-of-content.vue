da4944f7a3df731c8ec7912fb70283adde773880
Josh Worden <josh@parameter1.com>
2021-04-01 12:37:31 -0500

    Move data layer manipulation to marko components

--- packages/marko-web-native-x/browser/track-end-of-content.vue
+++ packages/marko-web-native-x/browser/track-end-of-content.vue
@@ -3,38 +3,14 @@
 </template>
 
 <script>
-import { get } from '@parameter1/base-cms-object-path';
 import { endOfContent } from '../utils/gtm-events';
 
 export default {
-  props: {
-    story: {
-      type: Object,
-      required: true,
-    },
-    path: {
-      type: String,
-      required: true,
-    },
-    preview: {
-      type: Boolean,
-      default: false,
-    },
-  },
   created() {
     this.observer = new IntersectionObserver((event) => {
       if (event[0].isIntersecting) {
         endOfContent(window);
       }
-    });
-    const dl = window.dataLayerNativeX || [];
-    dl.push({
-      story_id: this.story.id,
-      page_path: this.path,
-      page_title: this.story.title,
-      publisher_id: get(this.story, 'publisher.id'),
-      advertiser_id: get(this.story, 'advertiser.id'),
-      preview_mode: this.preview,
     });
   },
   mounted() {
