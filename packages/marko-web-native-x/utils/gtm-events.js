8587119ccaf7c9db2d19d8322d73f53ffc99f659
Josh Worden <josh@parameter1.com>
2021-04-01 12:41:43 -0500

    Only push events if the datalayer exists

--- packages/marko-web-native-x/utils/gtm-events.js
+++ packages/marko-web-native-x/utils/gtm-events.js
@@ -1,19 +1,19 @@
 export const pageLoad = (window) => {
-  const dataLayer = window.dataLayerNativeX || [];
-  dataLayer.push({ event: 'page_load' });
+  const dataLayer = window.dataLayerNativeX;
+  if (dataLayer) dataLayer.push({ event: 'page_load' });
 };
 
 export const outboundLink = (window, url) => {
-  const dataLayer = window.dataLayerNativeX || [];
-  dataLayer.push({ event: 'outbound_click', outbound_url: url, eventTimeout: 3000 });
+  const dataLayer = window.dataLayerNativeX;
+  if (dataLayer) dataLayer.push({ event: 'outbound_click', outbound_url: url, eventTimeout: 3000 });
 };
 
 export const share = (window, provider) => {
-  const dataLayer = window.dataLayerNativeX || [];
-  dataLayer.push({ event: 'share', social_provider: provider });
+  const dataLayer = window.dataLayerNativeX;
+  if (dataLayer) dataLayer.push({ event: 'share', social_provider: provider });
 };
 
 export const endOfContent = (window) => {
-  const dataLayer = window.dataLayerNativeX || [];
-  dataLayer.push({ event: 'scroll_to_bottom' });
+  const dataLayer = window.dataLayerNativeX;
+  if (dataLayer) dataLayer.push({ event: 'scroll_to_bottom' });
 };
