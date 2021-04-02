98187f7d0dd02b8f5bf36f902babc4772d2da3da
Josh Worden <josh@parameter1.com>
2021-04-02 10:17:27 -0500

    Don’t pass in the window global

--- packages/marko-web-native-x/utils/gtm-events.js
+++ packages/marko-web-native-x/utils/gtm-events.js
@@ -1,19 +1,20 @@
-export const pageLoad = (window) => {
+/* eslint-disable no-undef */
+export const pageLoad = () => {
   const dataLayer = window.dataLayerNativeX;
   if (dataLayer) dataLayer.push({ event: 'page_load' });
 };
 
-export const outboundLink = (window, url) => {
+export const outboundLink = (url) => {
   const dataLayer = window.dataLayerNativeX;
   if (dataLayer) dataLayer.push({ event: 'outbound_click', outbound_url: url, eventTimeout: 3000 });
 };
 
-export const share = (window, provider) => {
+export const share = (provider) => {
   const dataLayer = window.dataLayerNativeX;
   if (dataLayer) dataLayer.push({ event: 'share', social_provider: provider });
 };
 
-export const endOfContent = (window) => {
+export const endOfContent = () => {
   const dataLayer = window.dataLayerNativeX;
   if (dataLayer) dataLayer.push({ event: 'scroll_to_bottom' });
 };
