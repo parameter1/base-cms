b1904dfe9e2adce9c4f259ea2c23318ff5941469
Jacob Bare <jacob@parameter1.com>
2021-04-21 13:51:49 -0400

    Use browser component queue and load script on ready

--- packages/marko-web/browser/index.js
+++ packages/marko-web/browser/index.js
@@ -61,10 +61,28 @@ const registerComponent = (name, Component, provide) => {
   register(name, Component, { provide });
 };
 
-export default {
+const methods = {
   load,
   register,
   loadComponent,
   registerComponent,
+};
+
+let initialized = false;
+const init = () => {
+  if (initialized) return;
+  const { markoCompQueue } = window;
+  if (!markoCompQueue) throw new Error('Unable to load Marko Web components queue!');
+
+  for (let i = 0; i < markoCompQueue.length; i += 1) {
+    const [method, args] = markoCompQueue[i];
+    if (methods[method]) methods[method](...args);
+  }
+  initialized = true;
+};
+
+export default {
+  ...methods,
+  init,
   EventBus,
 };
