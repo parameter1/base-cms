bad4f9f1340a75ebdda8e0a40888a762a74a785e
Jacob Bare <jacob@parameter1.com>
2021-04-22 09:55:02 -0400

    Add dynamic import to all components and export

--- packages/marko-web/browser/components/index.js
+++ packages/marko-web/browser/components/index.js
@@ -1,10 +1,9 @@
-import LoadMoreTrigger from './load-more-trigger.vue';
-import TriggerInViewEvent from './trigger-in-view-event.vue';
-import TriggerScreenChangeEvent from './trigger-screen-change-event.vue';
-
-const OEmbed = () => import(/* webpackChunkName: "oembed" */ './oembed.vue');
-const FormDotComGatedDownload = () => import(/* webpackChunkName: "form-dot-com" */ './gated-download/form-dot-com.vue');
-const WufooGatedDownload = () => import(/* webpackChunkName: "wufoo-gated-download" */ './gated-download/wufoo.vue');
+export const LoadMoreTrigger = () => import(/* webpackChunkName: "load-more-trigger" */ './load-more-trigger.vue');
+export const TriggerInViewEvent = () => import(/* webpackChunkName: "trigger-in-view-event" */ './trigger-in-view-event.vue');
+export const TriggerScreenChangeEvent = () => import(/* webpackChunkName: "trigger-screen-change-event" */ './trigger-screen-change-event.vue');
+export const OEmbed = () => import(/* webpackChunkName: "oembed" */ './oembed.vue');
+export const FormDotComGatedDownload = () => import(/* webpackChunkName: "form-dot-com" */ './gated-download/form-dot-com.vue');
+export const WufooGatedDownload = () => import(/* webpackChunkName: "wufoo-gated-download" */ './gated-download/wufoo.vue');
 
 export default (Browser) => {
   Browser.register('LoadMoreTrigger', LoadMoreTrigger);
