5684ce06011f80e48923d8ef243519feb6117f03
Jacob Bare <jacob@parameter1.com>
2021-04-22 12:25:42 -0400

    Remove playground site

--- services/site/browser/index.js
+++
@@ -1,3 +0,0 @@
-import Browser from '@parameter1/base-cms-marko-web/browser';
-
-export default Browser;
--- services/site/server/routes/index.js
+++
@@ -1,5 +0,0 @@
-const index = require('../templates/index');
-
-module.exports = (app) => {
-  app.get('/', (_, res) => { res.marko(index); });
-};
