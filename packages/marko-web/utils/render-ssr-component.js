44c837e7b6f7efad508035e4ec8c8ca6ff55eac5
Jacob Bare <jacob@parameter1.com>
2021-06-23 10:32:56 -0500

    Add support for server rendering of Vue/browser componetns

---
+++ packages/marko-web/utils/render-ssr-component.js
@@ -0,0 +1,13 @@
+const { get } = require('@parameter1/base-cms-object-path');
+const Vue = require('vue');
+const VueServerRenderer = require('vue-server-renderer');
+
+const renderer = VueServerRenderer.createRenderer();
+
+module.exports = async (Component, { id, props } = {}) => {
+  if (get(Component, 'props.id')) throw new Error('The provided component has an `id` prop which is incompatible with SSR rendering.');
+  const app = new Vue({
+    render: h => h(Component, { attrs: { id }, props }),
+  });
+  return renderer.renderToString(app);
+};
