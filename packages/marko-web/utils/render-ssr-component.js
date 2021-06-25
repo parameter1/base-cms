a1fdfb4091b93e86e428b56f49327deaeced1eef
Jacob Bare <jacob@parameter1.com>
2021-06-25 08:47:40 -0500

    Use consistent event bus instance

--- packages/marko-web/utils/render-ssr-component.js
+++ packages/marko-web/utils/render-ssr-component.js
@@ -3,11 +3,12 @@ const Vue = require('vue');
 const VueServerRenderer = require('vue-server-renderer');
 
 const renderer = VueServerRenderer.createRenderer();
+const EventBus = new Vue();
 
 module.exports = async (Component, { id, props } = {}) => {
   if (get(Component, 'props.id')) throw new Error('The provided component has an `id` prop which is incompatible with SSR rendering.');
   const app = new Vue({
-    provide: { EventBus: new Vue() },
+    provide: { EventBus },
     render: h => h(Component, { attrs: { id }, props }),
   });
   return renderer.renderToString(app);
