9af39af93c6471dc5f2cf06f9dbfeded03b08c8c
Jacob Bare <jacob@parameter1.com>
2021-07-23 13:00:57 -0500

    Update base apollo package to use generic factory

--- packages/express-apollo/src/create-client.js
+++ packages/express-apollo/src/create-client.js
@@ -1,25 +1,10 @@
-const fetch = require('node-fetch');
-const { ApolloClient } = require('apollo-client');
-const { InMemoryCache } = require('apollo-cache-inmemory');
-const { createHttpLink } = require('apollo-link-http');
-const { setContext } = require('apollo-link-context');
+const createClient = require('@parameter1/base-cms-apollo-ssr');
 const fragmentMatcher = require('@parameter1/base-cms-graphql-fragment-types/fragment-matcher');
 
-const rootConfig = {
-  connectToDevTools: false,
-  ssrMode: true,
-};
-
-module.exports = (uri, config, linkConfig, contextFn) => {
-  const contextLink = setContext((ctx) => {
-    if (typeof contextFn === 'function') return contextFn(ctx);
-    return undefined;
-  });
-  const httpLink = createHttpLink({ fetch, ...linkConfig, uri });
-  return new ApolloClient({
-    ...config,
-    ...rootConfig,
-    link: contextLink.concat(httpLink),
-    cache: new InMemoryCache({ fragmentMatcher }),
-  });
-};
+module.exports = (uri, config, linkConfig, contextFn) => createClient({
+  uri,
+  config,
+  linkConfig,
+  contextFn,
+  cacheConfig: { fragmentMatcher },
+});
