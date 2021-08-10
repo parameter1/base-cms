7d08c4334d06292e8a5982c185f2be91661071e7
Jacob Bare <jacob@parameter1.com>
2021-08-10 15:48:30 -0500

    Create Omeda+Alchemer (SurveyGizo) form prefill

--- packages/marko-web-p1-fii/browser/index.js
+++ packages/marko-web-p1-fii/browser/index.js
@@ -1,5 +1,7 @@
 const OmedaWufooForm = () => import(/* webpackChunkName: "p1-fii-omeda-wufoo-form" */ './omeda-wufoo-form.vue');
+const OmedaAlchemerForm = () => import(/* webpackChunkName: "p1-fii-omeda-alchemer-form" */ './omeda-alchemer-form.vue');
 
 export default (Browser) => {
+  Browser.register('P1FIIOmedaAlchemerForm', OmedaAlchemerForm);
   Browser.register('P1FIIOmedaWufooForm', OmedaWufooForm);
 };
