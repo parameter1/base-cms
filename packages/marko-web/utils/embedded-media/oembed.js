28af8d335d26e199e33b37e5116ad0317867fc56
Shinsina <shinsina@att.net>
2021-08-31 14:41:31 -0500

    Utilize new handlers within core oEmbed handler.

--- packages/marko-web/utils/embedded-media/oembed.js
+++ packages/marko-web/utils/embedded-media/oembed.js
@@ -1,8 +1,18 @@
 const BrowserComponent = require('../../components/browser-component');
+const facebook = require('./facebook-oembed');
+const instagram = require('./instagram-oembed');
 
 module.exports = (tag, $global) => {
   const { config } = $global;
   const url = tag.id;
+  const facebookPost = facebook(url);
+  const instagramPost = instagram(url);
   const props = { mountPoint: config.oembedMountPoint(), url, attrs: tag.attrs };
+  if (facebookPost) {
+    return facebookPost;
+  }
+  if (instagramPost) {
+    return instagramPost;
+  }
   return BrowserComponent.renderToString({ $global, name: 'OEmbed', props });
 };
