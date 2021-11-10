dc528a9ce3f6ef38b0dae72558d982da7678e0a5
Jacob Bare <jacob@parameter1.com>
2021-11-10 12:42:37 -0600

    Support sending demographics on rapid ident

--- packages/marko-web-omeda-identity-x/rapid-identify.js
+++ packages/marko-web-omeda-identity-x/rapid-identify.js
@@ -1,5 +1,5 @@
 const gql = require('graphql-tag');
-const { get } = require('@parameter1/base-cms-object-path');
+const { get, getAsArray } = require('@parameter1/base-cms-object-path');
 
 
 const RAPID_IDENT = gql`
@@ -58,6 +58,24 @@ module.exports = async ({
     alpha3 = await getAlpha3CodeFor(countryCode, identityX);
   }
 
+  const demographics = getAsArray(appUser, 'customSelectFieldAnswers').filter((select) => {
+    const { field, hasAnswered } = select;
+    if (!field.externalId || !hasAnswered) return false;
+    const { namespace, identifier } = field.externalId;
+    return namespace.provider === 'omeda'
+      && namespace.tenant === brandKey.toLowerCase()
+      && namespace.type === 'demographic'
+      && parseInt(identifier.value, 10)
+      && select.answers.some(answer => answer.externalIdentifier);
+  }).map((select) => {
+    const { field } = select;
+    const { identifier } = field.externalId;
+    return {
+      id: parseInt(identifier.value, 10),
+      values: select.answers.map(answer => answer.externalIdentifier).filter(v => v),
+    };
+  });
+
   const input = {
     productId,
     email: appUser.email,
@@ -68,6 +86,7 @@ module.exports = async ({
     ...(alpha3 && { countryCode: alpha3 }),
     ...(regionCode && { regionCode }),
     ...(postalCode && { postalCode }),
+    ...(demographics.length && { demographics }),
   };
   const { data } = await omedaGraphQL.mutate({
     mutation: RAPID_IDENT,
