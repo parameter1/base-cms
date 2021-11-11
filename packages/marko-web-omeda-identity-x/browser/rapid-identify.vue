4fb708c40867bf6ac1f81696973f4f1229d00c75
Jacob Bare <jacob@parameter1.com>
2021-11-11 09:38:29 -0600

    Move rapid identify cookie handling to server-side

--- packages/marko-web-omeda-identity-x/browser/rapid-identify.vue
+++ packages/marko-web-omeda-identity-x/browser/rapid-identify.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-const { warn } = console;
+const { error } = console;
 
 export default {
   props: {
@@ -11,70 +11,19 @@ export default {
       type: String,
       default: '/__idx/omeda-rapid-ident',
     },
-    olyticsCookieName: {
-      type: String,
-      default: 'oly_enc_id',
-    },
-    idxCookieName: {
-      type: String,
-      default: '__idx',
-    },
-  },
-
-  data: () => ({
-    cookies: null,
-  }),
-
-  computed: {
-    hasIdentityXUser() {
-      return Boolean(this.cookies[this.idxCookieName]);
-    },
-    encryptedId() {
-      const id = this.cookies[this.olyticsCookieName];
-      // the encrypted ID cookie value is enclosed with double-quotes. remove.
-      return id ? id.replace(/"/g, '') : null;
-    },
   },
 
   created() {
-    this.parseCookies();
     this.rapidIdentify();
   },
 
   methods: {
     async rapidIdentify() {
-      if (this.hasIdentityXUser) {
-        const res = await fetch(this.endpoint, { method: 'GET' });
-        const { encryptedId } = await res.json();
-        if (encryptedId) this.$emit('encrypted-id-found', encryptedId);
-        if (encryptedId && encryptedId !== this.encryptedId) {
-          // run olytics confirm to set the IdentityX user's omeda ID.
-          const { olytics } = window;
-          if (olytics) {
-            olytics.confirm(encryptedId);
-          } else {
-            warn('Unable to identify IdentityX user: no olytics instance was found.');
-          }
-        }
+      try {
+        await fetch(this.endpoint, { method: 'GET' });
+      } catch (e) {
+        error('IdentityX to Omeda identification failed.', e);
       }
-    },
-
-    parseCookies() {
-      if (!this.cookies) {
-        this.cookies = document.cookie.split(';').reduce((o, pairs) => {
-          const [key, value] = pairs.trim().split('=').map(v => decodeURIComponent(v));
-          let v;
-          switch (value) {
-            case 'null': v = null; break;
-            case 'true': v = true; break;
-            case 'false': v = false; break;
-            case 'undefined': v = undefined; break;
-            default: v = value;
-          }
-          return { ...o, [key]: v };
-        }, {});
-      }
-      return this.cookies;
     },
   },
 };
--- packages/marko-web-omeda-identity-x/routes/rapid-identify.js
+++ packages/marko-web-omeda-identity-x/routes/rapid-identify.js
@@ -2,6 +2,10 @@ const { Router } = require('express');
 const { getAsObject } = require('@parameter1/base-cms-object-path');
 const { asyncRoute } = require('@parameter1/base-cms-utils');
 const jsonErrorHandler = require('@parameter1/base-cms-marko-web/express/json-error-handler');
+
+const tokenCookie = require('@parameter1/base-cms-marko-web-identity-x/utils/token-cookie');
+const olyticsCookie = require('@parameter1/base-cms-marko-web-omeda/olytics/customer-cookie');
+
 const omedaRapidIdentityX = require('../rapid-identify');
 const findEncryptedId = require('../external-id/find-encrypted-customer-id');
 
@@ -10,11 +14,22 @@ module.exports = ({ brandKey, productId } = {}) => {
   if (!productId) throw new Error('An Omeda rapid identification product ID is required to use this middleware.');
   const router = Router();
   router.get('/', asyncRoute(async (req, res) => {
-    const data = { encryptedId: null, source: null };
+    const data = {
+      userId: null,
+      encryptedId: null,
+      source: null,
+      didResetCookie: false,
+    };
+    const idxUserExists = tokenCookie.exists(req);
+    if (!idxUserExists) return res.json(data);
+
+
     const { identityX } = req;
     const context = await identityX.loadActiveContext();
     const user = getAsObject(context, 'user');
     if (!user.id) return res.json(data);
+    data.userId = user.id;
+
     // determine if an encrypted ID already exists for this user and brand.
     const encryptedId = findEncryptedId({ externalIds: user.externalIds, brandKey });
     if (encryptedId) {
@@ -31,6 +46,15 @@ module.exports = ({ brandKey, productId } = {}) => {
       });
       data.encryptedId = encryptedCustomerId;
       data.source = 'new';
+    }
+
+    // finally, reset the olytics cookie if it isn't present
+    // or doesn't match the current encrypted id.
+    const currentEncId = olyticsCookie.parseFrom(req);
+
+    if (!currentEncId || currentEncId !== data.encryptedId) {
+      olyticsCookie.setTo(res, data.encryptedId);
+      data.didResetCookie = true;
     }
     return res.json(data);
   }));
