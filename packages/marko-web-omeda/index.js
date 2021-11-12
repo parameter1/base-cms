e27684a875837d0b7ddc81d94012b2005bfe9451
Jacob Bare <jacob@parameter1.com>
2021-11-12 15:05:23 -0600

    Move installation of middlewares to root export

--- packages/marko-web-omeda/index.js
+++ packages/marko-web-omeda/index.js
@@ -1,1 +1,24 @@
-module.exports = {};
+const { graphqlClient, rapidIdentify } = require('./middleware');
+
+module.exports = (app, {
+  brandKey,
+  clientKey,
+  appId,
+  inputId,
+  rapidIdentProductId,
+  omedaGraphQLClientProp = '$omedaGraphQLClient',
+  omedaRapidIdentifyProp = '$omedaRapidIdentify',
+} = {}) => {
+  app.use(graphqlClient({
+    brandKey,
+    clientKey,
+    appId,
+    inputId,
+    prop: omedaGraphQLClientProp,
+  }));
+  app.use(rapidIdentify({
+    productId: rapidIdentProductId,
+    omedaGraphQLClientProp,
+    prop: omedaRapidIdentifyProp,
+  }));
+};
--- packages/marko-web-omeda/middleware/index.js
+++ packages/marko-web-omeda/middleware/index.js
@@ -1,19 +1,4 @@
 const graphqlClient = require('./graphql-client');
 const rapidIdentify = require('./rapid-identify');
 
-module.exports = (app, {
-  brandKey,
-  clientKey,
-  appId,
-  inputId,
-
-  rapidIdentProductId,
-} = {}) => {
-  app.use(graphqlClient({
-    brandKey,
-    clientKey,
-    appId,
-    inputId,
-  }));
-  app.use(rapidIdentify({ productId: rapidIdentProductId }));
-};
+module.exports = { graphqlClient, rapidIdentify };
