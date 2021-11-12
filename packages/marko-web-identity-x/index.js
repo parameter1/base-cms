dd5b44c6453726aa19d12131c022d7a003466aa3
Jacob Bare <jacob@parameter1.com>
2021-11-12 15:45:41 -0600

    Add doc blocks

--- packages/marko-web-identity-x/index.js
+++ packages/marko-web-identity-x/index.js
@@ -1,6 +1,11 @@
 const routes = require('./routes');
 const middleware = require('./middleware');
 
+/**
+ *
+ * @param {object} app The express app instance
+ * @param {IdentityXConfiguration} config The IdentityX config
+ */
 module.exports = (app, config) => {
   app.use(middleware(config));
   app.use('/__idx', routes);
