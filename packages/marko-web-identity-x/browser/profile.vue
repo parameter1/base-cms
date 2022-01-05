1cfbd84b08d6bb8d959a0d953fa0b500b12c7fe9
Brian Miller <brian@parameter1.com>
2022-01-05 11:42:21 -0600

    Add customBoolean support to profile componet

--- packages/marko-web-identity-x/browser/profile.vue
+++ packages/marko-web-identity-x/browser/profile.vue
@@ -73,6 +73,23 @@
           />
         </div>
 
+        <div v-if="customBooleanFieldAnswers.length" class="row">
+          <div
+            v-for="fieldAnswer in customBooleanFieldAnswers"
+            :key="fieldAnswer.id"
+            class="col-12"
+          >
+            <custom-boolean
+              :id="fieldAnswer.id"
+
+              :message="fieldAnswer.field.label"
+              :required="fieldAnswer.field.required"
+              :value="fieldAnswer.value"
+              @input="onCustomBooleanChange(fieldAnswer.id, $event)"
+            />
+          </div>
+        </div>
+
         <div v-if="emailConsentRequest" class="row mt-3">
           <div class="col-md-6">
             <receive-email
@@ -133,6 +150,7 @@ import post from './utils/post';
 import cookiesEnabled from './utils/cookies-enabled';
 import regionCountryCodes from './utils/region-country-codes';
 
+import CustomBoolean from './form/fields/custom-boolean.vue';
 import CustomSelect from './form/fields/custom-select.vue';
 import GivenName from './form/fields/given-name.vue';
 import FamilyName from './form/fields/family-name.vue';
@@ -152,6 +170,7 @@ const { isArray } = Array;
 
 export default {
   components: {
+    CustomBoolean,
     CustomSelect,
     GivenName,
     FamilyName,
@@ -287,6 +306,14 @@ export default {
     /**
      *
      */
+    customBooleanFieldAnswers() {
+      const { customBooleanFieldAnswers } = this.user;
+      return isArray(customBooleanFieldAnswers) ? customBooleanFieldAnswers : [];
+    },
+
+    /**
+     *
+     */
     customSelectFieldAnswers() {
       const { customSelectFieldAnswers } = this.user;
       return isArray(customSelectFieldAnswers) ? customSelectFieldAnswers : [];
@@ -343,6 +370,10 @@ export default {
       } else {
         this.user.regionalConsentAnswers.push({ id: policyId, given });
       }
+    },
+
+    onCustomBooleanChange(fieldId, newValue) {
+      this.user.customBooleanFieldAnswers = this.customBooleanFieldAnswers;
     },
 
     onCustomSelectChange(answers, ids) {
