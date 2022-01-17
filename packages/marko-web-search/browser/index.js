24d9e981db880ab2bd1425b9380446430da7a99e
Shinsina <shinsina@att.net>
2022-01-17 13:52:11 -0600

    Create, Register, Use new Vue dropdown component.

--- packages/marko-web-search/browser/index.js
+++ packages/marko-web-search/browser/index.js
@@ -1,7 +1,9 @@
 const ToggleFilterContainer = () => import(/* webpackChunkName: "marko-web-search-toggle-filter-container" */ './toggle-filters-button.vue');
 const ToggleFilter = () => import(/* webpackChunkName: "marko-web-search-toggle-filter" */ './toggle-filter-button.vue');
+const SortSelect = () => import(/* webpackChunkName: "marko-web-search-sort-select" */ './sort-select.vue');
 
 export default (Browser) => {
   Browser.register('MarkoWebSearchToggleFilterContainer', ToggleFilterContainer);
   Browser.register('MarkoWebSearchToggleFilter', ToggleFilter);
+  Browser.register('MarkoWebSearchSortSelect', SortSelect);
 };
---
+++ packages/marko-web-search/browser/sort-select.vue
@@ -0,0 +1,39 @@
+<template>
+  <select :class="className" @change="onChange">
+    <option
+      v-for="item in nodes"
+      :key="item.id"
+      :selected="item.isSelected"
+      :value="item.id"
+    >
+      Sort: {{ item.label }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    className: {
+      type: String,
+      default: 'marko-web-search-sort-select',
+    },
+    nodes: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    onChange(e) {
+      if (e.target.options.selectedIndex > -1) {
+        const selected = e.target.options[e.target.options.selectedIndex];
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const params = Object.fromEntries(urlSearchParams.entries());
+        params.sortBy = selected.value;
+        const newUrlSearchParams = new URLSearchParams({ ...params });
+        window.location.href = `${window.location.origin}/search?${newUrlSearchParams.toString()}`;
+      }
+    },
+  },
+};
+</script>
