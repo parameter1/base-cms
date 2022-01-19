77b8321517cc26ae0ffc4f654f7aaa92cb5e1fe1
Jacob Bare <jacob@parameter1.com>
2022-01-19 14:00:51 -0600

    Pass omeda client key

--- services/example-website/config/omeda.js
+++ services/example-website/config/omeda.js
@@ -1,5 +1,6 @@
 module.exports = {
   brandKey: process.env.OMEDA_BRAND_KEY,
+  clientKey: process.env.OMEDA_CLIENT_KEY,
   appId: process.env.OMEDA_APP_ID,
   inputId: process.env.OMEDA_INPUT_ID,
   graphqlUri: 'https://graphql.omeda.parameter1.com/',
