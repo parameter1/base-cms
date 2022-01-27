960931cc18dd5857905bfc5e9708879fc6cc3670
Shinsina <shinsina@att.net>
2022-01-27 10:39:14 -0600

    Additional logic for rendering + URL param updates

--- packages/marko-web-search/browser/sort-select.vue
+++ packages/marko-web-search/browser/sort-select.vue
@@ -1,5 +1,5 @@
 <template>
-  <select class="custom-select" @change="onChange">
+  <select v-if="searchQuery" class="custom-select" @change="onChange">
     <option
       v-for="option in options"
       :key="option.id"
@@ -22,13 +22,34 @@ export default {
       type: String,
       default: 'PUBLISHED',
     },
+    searchQuery: {
+      type: Boolean,
+      default: true,
+    },
+    defaultWithSearchQuery: {
+      type: String,
+      default: 'PUBLISHED',
+    },
+  },
+  mounted() {
+    const url = new URL(window.location);
+    const params = new URLSearchParams(window.location.search);
+    if (!this.searchQuery && params.sortField) {
+      delete params.sortField;
+      url.search = `${params}`;
+      window.location.href = `${url}`;
+    }
   },
   methods: {
     onChange(event) {
       const { value } = event.target;
       const url = new URL(window.location);
       const params = new URLSearchParams(window.location.search);
-      params.set('sortField', value);
+      if ((value !== this.defaultWithSearchQuery)) {
+        params.set('sortField', value);
+      } else {
+        params.delete('sortField');
+      }
       url.search = `${params}`;
       window.location.href = `${url}`;
     },
