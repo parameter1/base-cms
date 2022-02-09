804a244a7be4a7952d9ddc0c6ab9d2647be2dacb
Shinsina <shinsina@att.net>
2022-02-09 12:45:59 -0600

    Modify query logic to not return home.

--- packages/utils/src/get-section-from-schedules.js
+++ packages/utils/src/get-section-from-schedules.js
@@ -3,12 +3,12 @@ module.exports = async ({ content, siteId, load }) => {
   const query = {
     status: 1,
     'site.$id': siteId,
+    alias: { $ne: 'home' },
   };
   if (sectionQuery.length) {
     const sectionsFromScheds = sectionQuery.filter(schedule => schedule.siteId === siteId);
     const foundSections = await Promise.all(sectionsFromScheds.map(section => load('websiteSection', section.sectionId, {}, query)));
-    const currentSiteSection = foundSections.find(({ alias }) => alias !== 'home');
-    if (currentSiteSection) return currentSiteSection;
+    if (foundSections) return foundSections[0];
     return null;
   }
   return null;
