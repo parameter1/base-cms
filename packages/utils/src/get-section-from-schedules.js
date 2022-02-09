2d1f2fa37dd298ed64c8f2d94b18e1cbe0d7ae4e
Shinsina <shinsina@att.net>
2022-02-09 11:58:49 -0600

    Create utility function.

---
+++ packages/utils/src/get-section-from-schedules.js
@@ -0,0 +1,16 @@
+module.exports = async ({ content, siteId, load }) => {
+  const { sectionQuery } = content;
+  const query = {
+    status: 1,
+    'site.$id': siteId,
+  };
+  if (sectionQuery.length) {
+    const sectionFromSched = sectionQuery.find(({ siteId: schedSiteId }) => siteId === schedSiteId);
+    if (sectionFromSched) {
+      const currentSiteSection = await load('websiteSection', sectionFromSched.sectionId, { alias: 1 }, query);
+      if (currentSiteSection) return currentSiteSection;
+    }
+    return null;
+  }
+  return null;
+};
--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -9,6 +9,7 @@ const compareNumbers = require('./compare-numbers');
 const getDefaultTaxonomyTypes = require('./get-default-taxonomy-types');
 const getDefaultContentTypes = require('./get-default-content-types');
 const getPublishedContentCriteria = require('./get-published-content-criteria');
+const getSectionFromSchedules = require('./get-section-from-schedules');
 const isDev = require('./is-dev');
 const isFunction = require('./is-function');
 const isObject = require('./is-object');
@@ -30,6 +31,7 @@ module.exports = {
   getDefaultTaxonomyTypes,
   getDefaultContentTypes,
   getPublishedContentCriteria,
+  getSectionFromSchedules,
   isDev,
   isFunction,
   isObject,
