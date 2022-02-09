dd63c978bc316d68dcb029d42e8fafecab00bed8
Shinsina <shinsina@att.net>
2022-02-09 12:35:21 -0600

    Do not return home.

--- packages/utils/src/get-section-from-schedules.js
+++ packages/utils/src/get-section-from-schedules.js
@@ -5,11 +5,13 @@ module.exports = async ({ content, siteId, load }) => {
     'site.$id': siteId,
   };
   if (sectionQuery.length) {
-    const sectionFromSched = sectionQuery.find(({ siteId: schedSiteId }) => siteId === schedSiteId);
-    if (sectionFromSched) {
-      const currentSiteSection = await load('websiteSection', sectionFromSched.sectionId, {}, query);
-      if (currentSiteSection) return currentSiteSection;
-    }
+    const sectionsFromScheds = sectionQuery.map((schedule) => {
+      if (schedule.siteId === siteId) return schedule;
+      return null;
+    }).filter(v => v);
+    const foundSections = await Promise.all([sectionsFromScheds.map(section => load('websiteSection', section.sectionId, {}, query))]);
+    const currentSiteSection = foundSections.find(({ alias }) => alias !== 'home');
+    if (currentSiteSection) return currentSiteSection;
     return null;
   }
   return null;
