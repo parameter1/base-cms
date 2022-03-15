aca5113cf07aba857f30745568c448c8c9bb3d0f
Shinsina <shinsina@att.net>
2022-03-15 10:57:11 -0500

    Add newsletter.js file to email resolver index.js

--- services/graphql-server/src/graphql/resolvers/email/index.js
+++ services/graphql-server/src/graphql/resolvers/email/index.js
@@ -1,9 +1,11 @@
 const deepAssign = require('deep-assign');
 
 const campaign = require('./campaign');
+const newsletter = require('./newsletter');
 const schedule = require('./schedule');
 
 module.exports = deepAssign(
   campaign,
+  newsletter,
   schedule,
 );
