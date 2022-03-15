8b289e89ff50690aa0aa0ac2e7d939218aef6fbc
Josh Worden <josh@parameter1.com>
2022-03-15 12:19:53 -0500

    Remove clean and cookie stickiness

--- packages/marko-web-omeda-identity-x/middleware/set-promo-source.js
+++ packages/marko-web-omeda-identity-x/middleware/set-promo-source.js
@@ -1,10 +1,5 @@
-const clean = (value) => {
-  if (!value || value === 'null') return null;
-  return value.replace(/"'/g, '').trim().toUpperCase();
-};
-
 /**
- * Sets the `omeda_promo_code` cookie to identify the customer's original promotion source if the
+ * Sets the `omeda_promo_code` cookie to identify the customer's promotion source if the
  * `omeda_promo_code` URL parameter is present.
  * '
  * @param {String} omedaPromoCodeCookieName The name of the cookie to interact with
@@ -12,15 +7,13 @@ const clean = (value) => {
 module.exports = ({
   omedaPromoCodeCookieName = 'omeda_promo_code',
 } = {}) => (req, res, next) => {
-  const promoSource = clean(req.cookies[omedaPromoCodeCookieName]);
-
   const { [omedaPromoCodeCookieName]: promoCode } = req.query;
-  const incomingPromoCode = clean(promoCode);
+  const incomingPromoCode = `${promoCode || ''}`.trim().toUpperCase();
 
-  // Only set the promo source cookie if it doesn't already exist
-  if (!promoSource && incomingPromoCode) {
+  // Set the promo source cookie
+  if (incomingPromoCode) {
     const options = { maxAge: 60 * 60 * 24 * 365, httpOnly: false };
-    res.cookie(omedaPromoCodeCookieName, `${incomingPromoCode}`, options);
+    res.cookie(omedaPromoCodeCookieName, incomingPromoCode, options);
   }
 
   next();
