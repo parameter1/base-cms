c1a6024c85d627cc5ea50be5ad9e290a7f6be52f
Brian Miller <brian@parameter1.com>
2022-04-05 11:35:23 -0500

    add new profile view gtm tracker component

---
+++ packages/marko-web-identity-x/browser/gtm-tracker/profile-view.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="profile-view" />
+</template>
+
+<script>
+import { profileView } from '../../utils/gtm-events';
+
+export default {
+  /**
+   *
+   */
+  props: {
+    eventName: {
+      type: String,
+      default: 'profile_view',
+    },
+  },
+  created() {
+    this.observer = new IntersectionObserver((event) => {
+      if (event[0].isIntersecting) {
+        profileView({ event: this.eventName });
+      }
+    });
+  },
+  mounted() {
+    this.observer.observe(this.$el);
+  },
+};
+</script>
