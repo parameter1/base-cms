e7601b4138c4963604ca5ddc70fd5c841bb91fdb
Jacob Bare <jacob@parameter1.com>
2022-04-06 13:51:31 -0500

    Do not throw fatal error when images are missing/invalid
    Prevents the GraphQL API from fatally erroring when an image is not found

--- packages/image/src/create-src-for.js
+++ packages/image/src/create-src-for.js
@@ -6,6 +6,9 @@ module.exports = (host, image, options, defaultOptions) => {
     fileName,
     cropDimensions,
   } = image;
+  if (!fileName || !filePath) {
+    return buildImgixUrl(`https://${host}/asset-is-missing-file-name-or-path`, options, defaultOptions);
+  }
   const path = cropDimensions && cropDimensions.aspectRatio ? `${filePath}/${cropDimensions.aspectRatio}` : filePath;
   const src = `https://${host}/${path}/${fileName}`;
   return buildImgixUrl(src, options, defaultOptions);
--- services/graphql-server/src/graphql/utils/get-image-dimensions.js
+++ services/graphql-server/src/graphql/utils/get-image-dimensions.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const newrelic = require('../../newrelic');
 
 /**
  * Retrieves width/height information from Imgix and sets to
@@ -18,12 +19,17 @@ module.exports = async ({
   } = image;
   if (width && height) return { width, height };
   const url = `https://${host}/${filePath}/${fileName}?fm=json`;
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`Image at ${filePath}/${fileName} ${res.statusText} ID: ${image._id}`);
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Image at ${filePath}/${fileName} ${res.statusText} ID: ${image._id}`);
+    }
+    const { PixelWidth, PixelHeight } = await res.json();
+    const $set = { width: PixelWidth, height: PixelHeight };
+    await basedb.updateOne('platform.Asset', { _id: image._id }, { $set });
+    return { width: PixelWidth, height: PixelHeight };
+  } catch (e) {
+    newrelic.noticeError(e);
+    return { width: 0, height: 0 };
   }
-  const { PixelWidth, PixelHeight } = await res.json();
-  const $set = { width: PixelWidth, height: PixelHeight };
-  await basedb.updateOne('platform.Asset', { _id: image._id }, { $set });
-  return { width: PixelWidth, height: PixelHeight };
 };
