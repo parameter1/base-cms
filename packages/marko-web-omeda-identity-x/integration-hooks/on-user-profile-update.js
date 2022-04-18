04d2e2c20946cc1064be8c825f3a4b1a35d1af5f
Josh Worden <josh@parameter1.com>
2022-04-18 16:28:09 -0500

    Add promocode support to profile update

--- packages/marko-web-omeda-identity-x/integration-hooks/on-user-profile-update.js
+++ packages/marko-web-omeda-identity-x/integration-hooks/on-user-profile-update.js
@@ -1,9 +1,22 @@
+const extractPromoCode = require('../utils/extract-promo-code');
+
 module.exports = async ({
   idxOmedaRapidIdentifyProp = '$idxOmedaRapidIdentify',
+  omedaPromoCodeCookieName = 'omeda_promo_code',
+  omedaPromoCodeDefault,
   req,
   user,
 }) => {
   const idxOmedaRapidIdentify = req[idxOmedaRapidIdentifyProp];
   if (!idxOmedaRapidIdentify) throw new Error(`Unable to find the IdentityX+Omeda rapid identifier on the request using ${idxOmedaRapidIdentifyProp}`);
-  return idxOmedaRapidIdentify({ user });
+  const promoCode = extractPromoCode({
+    omedaPromoCodeCookieName,
+    omedaPromoCodeDefault,
+    cookies: req.cookies,
+  });
+
+  return idxOmedaRapidIdentify({
+    user,
+    ...(promoCode && { promoCode }),
+  });
 };
