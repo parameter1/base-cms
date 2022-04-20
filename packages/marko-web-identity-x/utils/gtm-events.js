08bdbc547e0ff9edab63e77296153963d54918f4
Brian Miller <brian@parameter1.com>
2022-04-20 08:27:38 -0500

    Remove use of datalayerIdentityX instead use standard datalayer

--- packages/marko-web-identity-x/utils/gtm-events.js
+++ packages/marko-web-identity-x/utils/gtm-events.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
 export const loginSubmit = ({ event = 'login' }) => {
-  const dataLayer = window.dataLayerIdentityX;
+  const { dataLayer } = window;
   if (dataLayer) dataLayer.push({ event });
 };
 
 export const profileView = ({ event = 'profile_view' }) => {
-  const dataLayer = window.dataLayerIdentityX;
+  const { dataLayer } = window;
   if (dataLayer) dataLayer.push({ event });
 };
 
 export const profileSubmit = ({ event = 'profile_submit', userId }) => {
-  const dataLayer = window.dataLayerIdentityX;
+  const { dataLayer } = window;
   if (dataLayer) dataLayer.push({ event, userId });
 };
