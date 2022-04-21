97f34ca554c6ac6150e17fb044f1f3abb83d8717
Josh Worden <josh@parameter1.com>
2022-04-21 12:42:45 -0500

    Remove old template

--- services/example-website/server/templates/user/index.js
+++ services/example-website/server/templates/user/index.js
@@ -1,13 +1,11 @@
 const authenticate = require('./authenticate');
 const login = require('./login');
 const logout = require('./logout');
-const register = require('./register');
 const profile = require('./profile');
 
 module.exports = {
   authenticate,
   login,
   logout,
-  register,
   profile,
 };
