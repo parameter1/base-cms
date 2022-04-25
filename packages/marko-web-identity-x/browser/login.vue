44a75551ae1a37c6021b1359626e3722b99d8fbd
Brian Miller <brian@parameter1.com>
2022-04-25 14:05:11 -0500

    Send eventLabel to /login post
    This will require https://github.com/parameter1/identity-x/pull/10

--- packages/marko-web-identity-x/browser/login.vue
+++ packages/marko-web-identity-x/browser/login.vue
@@ -182,6 +182,7 @@ export default {
         this.loading = true;
         const res = await post('/login', {
           email: this.email,
+          eventLabel: this.eventLabel,
           redirectTo: this.redirectTo,
           authUrl: this.authUrl,
           appContextId: this.appContextId,
