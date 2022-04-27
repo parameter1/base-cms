92a8c3dc6a4c5ad085fa561a6507642692a5eabe
Josh Worden <josh@parameter1.com>
2022-04-27 11:26:15 -0500

    remove defaults, json/cookie parsing

--- packages/marko-web-identity-x/browser/service.js
+++ packages/marko-web-identity-x/browser/service.js
@@ -1,27 +1,28 @@
 // It's 2022, why is there not a better way to do this?
 const getCookie = (key) => {
-  const cookies = document.cookie.split(/; */).reduce((obj, cookie) => {
-    const [name, value] = cookie.split('=').map(decodeURIComponent);
-    return { ...obj, [name]: value };
+  const cookies = document.cookie.split(';').reduce((obj, cookie) => {
+    const [name, value] = cookie.split('=').map(v => decodeURIComponent(v.trim()));
+    return { ...obj, ...(value && { [name]: value }) };
   }, {});
   return cookies[key];
 };
-const { warn } = console;
+
+const parseContext = () => {
+  try {
+    return JSON.parse(getCookie('__idx_ctx'));
+  } catch (e) {
+    return {};
+  }
+};
 
 export default class IdentityX {
   context = {};
 
   constructor() {
-    try {
-      // Read cookie
-      const cookie = getCookie('__idx_ctx');
-      this.context = JSON.parse(cookie || '{}');
-    } catch (e) {
-      warn('Unable to read IdentityX context from cookie');
-    }
+    this.context = parseContext();
   }
 
   getLoginSource() {
-    return this.context.loginSource || 'login';
+    return this.context.loginSource;
   }
 }
