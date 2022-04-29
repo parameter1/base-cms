fee06efc729dacc490baf1db7310822b868ec308
Brian Miller <brian@parameter1.com>
2022-04-29 09:37:17 -0500

    fix browser lint file

--- packages/marko-web-theme-monorail/browser/.eslintrc.js
+++ packages/marko-web-theme-monorail/browser/.eslintrc.js
@@ -1,1 +1,21 @@
-module.exports = require('../../../eslintrc.browser');
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
+    }],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
