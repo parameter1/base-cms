ed95cc0adb3ee072e2ca37c48d5c193590ceceb6
Jacob Bare <jacob@parameter1.com>
2022-05-19 09:57:11 -0500

    Catch sha1 errors and return empty hash

--- packages/leaders-program/src/graphql/sha1.js
+++ packages/leaders-program/src/graphql/sha1.js
@@ -1,12 +1,18 @@
+const { error } = console;
+
 export default async (string) => {
-  if (process.server) return null;
-  const buffer = new TextEncoder('utf-8').encode(string);
-  const digest = await crypto.subtle.digest('SHA-1', buffer);
-  const hexCodes = [];
-  const view = new DataView(digest);
-  for (let i = 0; i < view.byteLength; i += 1) {
-    const byte = view.getUint8(i).toString(16).padStart(2, '0');
-    hexCodes.push(byte);
+  try {
+    const buffer = new TextEncoder('utf-8').encode(string);
+    const digest = await crypto.subtle.digest('SHA-1', buffer);
+    const hexCodes = [];
+    const view = new DataView(digest);
+    for (let i = 0; i < view.byteLength; i += 1) {
+      const byte = view.getUint8(i).toString(16).padStart(2, '0');
+      hexCodes.push(byte);
+    }
+    return hexCodes.join('');
+  } catch (e) {
+    error('Unable to create SHA1 - GraphQL cache will be disabled.', e);
+    return null;
   }
-  return hexCodes.join('');
 };
