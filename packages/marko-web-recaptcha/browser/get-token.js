58e7be8bd26c0c1586c5190bb2dc9be508fe9dc7
Brian Miller <brian@parameter1.com>
2022-05-25 11:45:03 -0500

    Add error handeling for unset props

--- packages/marko-web-recaptcha/browser/get-token.js
+++ packages/marko-web-recaptcha/browser/get-token.js
@@ -1,6 +1,5 @@
-export default ({
-  siteKey,
-  action,
-} = {
-  action: 'formWithReCAPTCHASubmit',
-}) => window.grecaptcha.execute(siteKey, { action });
+export default ({ siteKey, action }) => {
+  if (!siteKey) throw new Error('The recaptcha site key is required');
+  if (!action) throw new Error('The recaptcha action is required');
+  return window.grecaptcha.execute(siteKey, { action });
+};
