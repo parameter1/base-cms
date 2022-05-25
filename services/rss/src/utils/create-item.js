2ae5b1757ca2581120fd43283ecc1ec89fdf0225
Shinsina <shinsina@att.net>
2022-05-25 14:26:41 -0500

    Refactor composition of author and media tags.

--- services/rss/src/utils/create-item.js
+++ services/rss/src/utils/create-item.js
@@ -18,32 +18,19 @@ module.exports = ({
   ];
   if (teaser) parts.push(`<description>${xml.encode(teaser)}</description>`);
   if (publishedDate) parts.push(`<pubDate>${publishedDate}</pubDate>`);
-  const authorNodes = authors.map(authorEdge => authorEdge.node);
-  let authorsString = '';
-  if (authorNodes.length) {
-    authorNodes.forEach((authorNode, index) => {
-      const currentAuthorString = `${authorNode.publicEmail} (${authorNode.firstName} ${authorNode.lastName})`;
-      if (authorNode.publicEmail) {
-        if (index) {
-          authorsString += `, ${currentAuthorString}`;
-        } else {
-          authorsString += currentAuthorString;
-        }
-      }
-    });
-    if (authorsString) parts.push(`<author>${authorsString}</author>`);
-  }
+  const authorStrings = authors.reduce(authorEdge => authorEdge.node).map((authorEdge) => {
+    const { node } = authorEdge;
+    return `${node.publicEmail} (${node.firstName} ${node.lastName})`;
+  });
+  parts.push(`<author>${authorStrings.join(', ')}</author>`);
   if (primarySection && primarySection.alias !== 'home') {
     parts.push(`<category domain="${website.origin}">${xml.encode(primarySection.fullName.replace('>', '/'))}</category>`);
   }
-  const imageNodes = images.map(imageEdge => imageEdge.node);
-  if (imageNodes.length) {
-    imageNodes.forEach((imageNode) => {
-      parts.push(`<media:content url=${imageNode.src} medium="image" />`);
-    });
-  }
-  if (embedSrc) {
-    parts.push(`<media:content url=${embedSrc} medium="video" />`);
-  }
+  const imageMediaTags = images.reduce(imageEdge => imageEdge.node).map((imageEdge) => {
+    const { node } = imageEdge;
+    return `<media:content url=${node.src} medium="image" />`;
+  });
+  parts.push(...imageMediaTags);
+  if (embedSrc) parts.push(`<media:content url=${embedSrc} medium="video" />`);
   return `<item>${parts.join('')}</item>`;
 };
