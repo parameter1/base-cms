5f2588f2a238c654462fd6d51ac6f63cf4c765ca
Jacob Bare <jacob@parameter1.com>
2022-05-25 09:43:05 -0500

    Adjust script loading to force on load

--- packages/marko-web-recaptcha/browser/load.js
+++ packages/marko-web-recaptcha/browser/load.js
@@ -1,32 +1,31 @@
-let recaptchaLoadPromise;
-// eslint-disable-next-line consistent-return
-const recaptchaLoad = siteKey => new Promise(((resolve) => {
-  const loadRecaptchaScript = () => {
-    window.onRecaptchaLoadCallback = () => {
-      window.dispatchEvent((new Event('recaptchaloaded')));
+const onWindowLoad = (callback) => {
+  if (document.readyState === 'complete') {
+    requestAnimationFrame(callback);
+  } else {
+    window.addEventListener('load', function fn() {
+      window.removeEventListener('load', fn);
+      requestAnimationFrame(callback);
+    });
+  }
+};
 
-      resolve();
-    };
 
-    const docScripts = document.getElementsByTagName('script')[0];
-    const newScript = document.createElement('script');
-
-    newScript.type = 'text/javascript';
-    newScript.async = !0;
-    newScript.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}&onload=onRecaptchaLoadCallback`;
-    docScripts.parentNode.insertBefore(newScript, docScripts);
+const loadScript = siteKey => new Promise(((resolve, reject) => {
+  const callback = () => {
+    window.onRecaptchaLoadCallback = resolve;
+    const body = document.getElementsByTagName('body')[0];
+    const script = document.createElement('script');
+    script.onerror = reject;
+    script.type = 'text/javascript';
+    script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}&onload=onRecaptchaLoadCallback`;
+    body.appendChild(script);
   };
 
-  if (document.readyState === 'complete') return loadRecaptchaScript();
-
-  if (window.attachEvent) {
-    window.attachEvent('onload', loadRecaptchaScript);
-  } else {
-    window.addEventListener('load', loadRecaptchaScript, !1);
-  }
+  onWindowLoad(callback);
 }));
 
 export default async ({ siteKey }) => {
-  if (!recaptchaLoadPromise) recaptchaLoadPromise = recaptchaLoad(siteKey);
-  return recaptchaLoadPromise;
+  if (!siteKey) throw new Error('The recaptcha site key is required to the load script.');
+  if (!window.recaptchaLoadPromise) window.recaptchaLoadPromise = loadScript(siteKey);
+  return window.recaptchaLoadPromise;
 };
