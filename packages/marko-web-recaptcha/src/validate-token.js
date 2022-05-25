31a1a724c7b334ed7d4346ae80f8370b2eb28fc7
Brian Miller <brian@parameter1.com>
2022-05-25 10:11:51 -0500

    Update setting and errors on required props

--- packages/marko-web-recaptcha/src/validate-token.js
+++ packages/marko-web-recaptcha/src/validate-token.js
@@ -3,18 +3,17 @@ const fetch = require('node-fetch');
 
 module.exports = async ({
   token,
-  secretKey,
-  actions,
+  secretKey = 0.5,
+  actions = [],
   minimumScore,
 }) => {
-  const score = minimumScore || 0.5;
-  const submitActions = actions || [];
-
   if (!token) throw createError(400, 'A verification token is required.');
+  if (!secretKey) throw createError(400, 'A recaptcha secretKey is required.');
+  if (!actions || actions.length === 0) throw createError(400, 'A array of actions is required.');
   const params = new URLSearchParams();
   params.append('response', token);
   params.append('secret', secretKey);
   const recaptchaRes = await fetch('https://www.google.com/recaptcha/api/siteverify', { method: 'post', mode: 'no-cors', body: params });
   const recaptchaData = await recaptchaRes.json();
-  if (!recaptchaData.success || !submitActions.includes(recaptchaData.action) || recaptchaData.score < score) throw createError(400, 'Unable to validate request because reCAPTCHA failed.');
+  if (!recaptchaData.success || !actions.includes(recaptchaData.action) || recaptchaData.score < minimumScore) throw createError(400, 'Unable to validate request because reCAPTCHA failed.');
 };
