09aaa05fee826bc12e7692eacb0681fd1fd7b128
Brian Miller <brian@parameter1.com>
2022-05-25 12:04:20 -0500

    remove commented out stuff

--- services/example-website/browser/index.js
+++ services/example-website/browser/index.js
@@ -1,16 +1,10 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
-// import OmedaIdentityX from '@parameter1/base-cms-marko-web-omeda-identity-x/browser';
-// import GTM from '@parameter1/base-cms-marko-web-gtm/browser';
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
-// import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 
 MonoRail(Browser);
-// GTM(Browser);
-// OmedaIdentityX(Browser);
 Leaders(Browser);
-// Inquiry(Browser);
 ContactUs(Browser);
 
 const { EventBus } = Browser;
