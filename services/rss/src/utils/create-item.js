77754e53dddfe4e5d5c111fcfba592211882e293
Shinsina <shinsina@att.net>
2022-05-25 14:48:13 -0500

    Utilize reduce strictly, remove .map() calls.

--- services/rss/src/utils/create-item.js
+++ services/rss/src/utils/create-item.js
@@ -18,19 +18,31 @@ module.exports = ({
   ];
   if (teaser) parts.push(`<description>${xml.encode(teaser)}</description>`);
   if (publishedDate) parts.push(`<pubDate>${publishedDate}</pubDate>`);
-  const authorStrings = authors.reduce(authorEdge => authorEdge.node).map((authorEdge) => {
-    const { node } = authorEdge;
-    return `${node.publicEmail} (${node.firstName} ${node.lastName})`;
-  });
-  parts.push(`<author>${authorStrings.join(', ')}</author>`);
+  const authorStrings = authors.reduce((arr, { node }) => {
+    const { publicEmail, firstName, lastName } = node;
+
+    const nameParts = [];
+    if (firstName) nameParts.push(firstName);
+    if (lastName) nameParts.push(firstName);
+
+    const authorParts = [];
+    if (publicEmail) authorParts.push(publicEmail);
+    if (nameParts.length) authorParts.push(nameParts.join(' '));
+
+    if (authorParts.length) arr.push(authorParts.join(' '));
+
+    return arr;
+  }, []);
+  if (authorStrings.length) parts.push(`<author>${authorStrings.join(', ')}</author>`);
   if (primarySection && primarySection.alias !== 'home') {
     parts.push(`<category domain="${website.origin}">${xml.encode(primarySection.fullName.replace('>', '/'))}</category>`);
   }
-  const imageMediaTags = images.reduce(imageEdge => imageEdge.node).map((imageEdge) => {
-    const { node } = imageEdge;
-    return `<media:content url=${node.src} medium="image" />`;
-  });
-  parts.push(...imageMediaTags);
-  if (embedSrc) parts.push(`<media:content url=${embedSrc} medium="video" />`);
+  const imageMediaTags = images.reduce((arr, { node }) => {
+    const { src } = node;
+    if (src) arr.push(`<media:content url="${node.src}" medium="image" />`);
+    return arr;
+  }, []);
+  if (imageMediaTags.length) parts.push(...imageMediaTags);
+  if (embedSrc) parts.push(`<media:content url="${embedSrc}" medium="video" />`);
   return `<item>${parts.join('')}</item>`;
 };
