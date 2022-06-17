d2c78976477d541a486ac0b6dd5350db2964dc45
Josh Worden <josh@parameter1.com>
2022-06-17 11:21:20 -0500

    Convert resync-customer-data to asyncRoute

--- packages/marko-web-omeda-identity-x/middleware/resync-customer-data.js
+++ packages/marko-web-omeda-identity-x/middleware/resync-customer-data.js
@@ -1,3 +1,4 @@
+const { asyncRoute } = require('@parameter1/base-cms-utils');
 const { getAsArray } = require('@parameter1/base-cms-object-path');
 const resyncCookie = require('../utils/resync-cookie');
 const findEncryptedId = require('../external-id/find-encrypted-customer-id');
@@ -15,47 +16,49 @@ const {
 module.exports = ({
   brandKey,
   omedaGraphQLClientProp = '$omedaGraphQLClient',
-}) => async (req, res, next) => {
+}) => asyncRoute(async (req, res, next) => {
   const { identityX, [omedaGraphQLClientProp]: omedaGraphQLClient } = req;
   if (!omedaGraphQLClient) throw new Error(`Unable to load the Omeda GraphQL API from the request using prop ${omedaGraphQLClientProp}`);
 
   // Load the user and find their external ID
   const { user } = await req.identityX.loadActiveContext();
-  const encryptedCustomerId = findEncryptedId({ externalIds: getAsArray(user, 'externalIds'), brandKey });
+  if (!user) return next();
 
   // Only check for the cookie if we have a valid IdX user and Omeda customer
-  if (user && encryptedCustomerId) {
-    const cookie = resyncCookie.parseFrom(req);
-    // If the cookie doesn't exist (or has expired), resync the user data
-    if (encryptedCustomerId && !cookie) {
-      // Load the customer data from Omeda and fields from IdentityX
-      const [omedaCustomer, omedaLinkedFields] = await Promise.all([
-        getOmedaCustomerRecord({ omedaGraphQLClient, encryptedCustomerId }),
-        getOmedaLinkedFields({ identityX, brandKey }),
-      ]);
+  const encryptedCustomerId = findEncryptedId({ externalIds: getAsArray(user, 'externalIds'), brandKey });
+  if (!encryptedCustomerId) return next();
 
-      // Update the IdentityX user record custom select fields with the Omeda customer demographics
-      await setOmedaDemographics({
-        req,
-        identityX,
-        user,
-        omedaCustomer,
-        fields: omedaLinkedFields.demographic,
-      });
+  // If the cookie doesn't exist (or has expired), resync the user data
+  const cookie = resyncCookie.parseFrom(req);
+  if (cookie) return next();
 
-      // Update the IdentityX user record custom boolean fields with the Omeda deployment opt-ins
-      await setOmedaDeploymentTypes({
-        identityX,
-        user,
-        omedaCustomer,
-        fields: omedaLinkedFields.deploymentType,
-      });
+  // Load the customer data from Omeda and fields from IdentityX
+  const [omedaCustomer, omedaLinkedFields] = await Promise.all([
+    getOmedaCustomerRecord({ omedaGraphQLClient, encryptedCustomerId }),
+    getOmedaLinkedFields({ identityX, brandKey }),
+  ]);
 
-      // Set the cookie to prevent further resyncs until interval is reached.
-      const value = { lastSync: Date.now() };
-      const maxAge = identityX.config.get('userResyncInterval', 7 * 24 * 60 * 60 * 1000); // 1 week
-      resyncCookie.setTo({ res, value, maxAge });
-    }
-  }
-  next();
-};
+  // Update the IdentityX user record custom select fields with the Omeda customer demographics
+  await setOmedaDemographics({
+    req,
+    identityX,
+    user,
+    omedaCustomer,
+    fields: omedaLinkedFields.demographic,
+  });
+
+  // Update the IdentityX user record custom boolean fields with the Omeda deployment opt-ins
+  await setOmedaDeploymentTypes({
+    identityX,
+    user,
+    omedaCustomer,
+    fields: omedaLinkedFields.deploymentType,
+  });
+
+  // Set the cookie to prevent further resyncs until interval is reached.
+  const value = { lastSync: Date.now() };
+  const maxAge = identityX.config.get('userResyncInterval', 7 * 24 * 60 * 60 * 1000); // 1 week
+  resyncCookie.setTo({ res, value, maxAge });
+
+  return next();
+});
