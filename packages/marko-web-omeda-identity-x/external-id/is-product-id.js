d640d883fd63c6474f3059c5403a1e03333eeda6
Josh Worden <josh@parameter1.com>
2022-07-06 15:51:39 -0500

    Add isProductId utility

---
+++ packages/marko-web-omeda-identity-x/external-id/is-product-id.js
@@ -0,0 +1,8 @@
+const isOmedaNamespace = require('./is-omeda-namespace');
+
+module.exports = ({ externalId, brandKey } = {}) => isOmedaNamespace({
+  externalId,
+  brandKey,
+  type: 'product',
+  valueMatcher: id => parseInt(id, 10),
+});
