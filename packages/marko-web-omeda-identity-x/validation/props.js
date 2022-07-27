b3c4da1174096442f66ee679f4a0e2d5a74cfccd
Josh Worden <josh@parameter1.com>
2022-07-27 14:53:28 -0500

    Export valid hooks from IdX package

--- packages/marko-web-omeda-identity-x/validation/props.js
+++ packages/marko-web-omeda-identity-x/validation/props.js
@@ -1,11 +1,13 @@
 const Joi = require('@parameter1/joi');
 const IdXConfig = require('@parameter1/base-cms-marko-web-identity-x/config');
+const validHooks = require('@parameter1/base-cms-marko-web-identity-x/hooks');
 
 module.exports = {
   behaviorId: Joi.number(),
   brandKey: Joi.string().trim().lowercase(),
   clientKey: Joi.string().trim().lowercase(),
   demographicId: Joi.number(),
+  hook: Joi.string().valid(...validHooks),
   idxConfig: Joi.object().instance(IdXConfig),
   promoCode: Joi.string(),
   valueId: Joi.number(),
