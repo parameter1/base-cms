51fa4265b73087ef934fa3db79bbe7d6de799446
Shinsina <shinsina@att.net>
2022-08-02 09:06:33 -0500

    Set fields as necessary, merge "changed" and old values as necessary.

--- services/graphql-server/src/graphql/resolvers/email/newsletter.js
+++ services/graphql-server/src/graphql/resolvers/email/newsletter.js
@@ -1,4 +1,5 @@
 const { Base4RestPayload } = require('@parameter1/base-cms-base4-rest-api');
+const { get } = require('@parameter1/base-cms-object-path');
 
 const validateRest = require('../../utils/validate-rest');
 
@@ -17,10 +18,27 @@ module.exports = {
         attributes,
       } = input;
 
+      const existingNewsletterProduct = await basedb.findOne('platform.Product', { _id: id }, { projection: { provider: 1 } });
+      const existingAttributes = get(existingNewsletterProduct, 'provider.attributes');
+      const mergedAttributes = {};
+      Object.keys(existingAttributes).forEach((attribute) => {
+        if (typeof attributes[attribute] !== 'undefined') {
+          mergedAttributes[attribute] = attributes[attribute];
+        } else {
+          mergedAttributes[attribute] = existingAttributes[attribute];
+        }
+      });
+      Object.keys(attributes).forEach((attribute) => {
+        if (!mergedAttributes[attribute] && typeof attributes[attribute] !== 'undefined') {
+          mergedAttributes[attribute] = attributes[attribute];
+        }
+      });
+      console.log(mergedAttributes);
+
       const body = new Base4RestPayload({ type });
-      body.set('provider.type', providerType);
-      body.set('provider.providerId', providerId);
-      body.set('provider.attributes', attributes);
+      if (providerType) body.set('provider.type', providerType);
+      if (providerId) body.set('provider.providerId', providerId);
+      if (Object.keys(mergedAttributes)) body.set('provider.attributes', mergedAttributes);
       body.set('id', id);
       await base4rest.updateOne({ model: type, id, body });
       return basedb.findOne('platform.Product', { _id: id });
