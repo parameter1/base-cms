87c8e15afca3caa40484d93924c77a76a5bc6565
Josh Worden <josh@parameter1.com>
2022-08-04 12:56:22 -0500

    add behavior schema

--- packages/marko-web-omeda-identity-x/validation/schemas/index.js
+++ packages/marko-web-omeda-identity-x/validation/schemas/index.js
@@ -1,6 +1,7 @@
 const appendBehavior = require('./append-behavior');
 const appendDemographic = require('./append-demographic');
 const appendPromoCode = require('./append-promo-code');
+const behavior = require('./behavior');
 const hookBehavior = require('./hook-behavior');
 const hookDemographic = require('./hook-demographic');
 const hookPromoCode = require('./hook-promo-code');
@@ -9,6 +10,7 @@ module.exports = {
   appendBehavior,
   appendDemographic,
   appendPromoCode,
+  behavior,
   hookBehavior,
   hookDemographic,
   hookPromoCode,
