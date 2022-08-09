e7bb3a491fcb1e7aeccfb6efddbb0aa4620ca201
Josh Worden <josh@parameter1.com>
2022-08-09 12:22:40 -0500

    Revert event-emitter change

--- packages/marko-web-identity-x/browser/mixins/global-event-emitter.js
+++ packages/marko-web-identity-x/browser/mixins/global-event-emitter.js
@@ -8,13 +8,7 @@ export default {
   },
   methods: {
     emit(name, data) {
-      const payload = {
-        ...data,
-        ...this.additionalEventData,
-        // Ensure the `actionSource` is emitted with client-side events
-        ...(this.source && { actionSource: this.source }),
-        ...(this.loginSource && { actionSource: this.loginSource }),
-      };
+      const payload = { ...data, ...this.additionalEventData };
       const { EventBus } = this;
       this.$emit(name, payload);
       EventBus.$emit(`identity-x-${name}`, payload);
