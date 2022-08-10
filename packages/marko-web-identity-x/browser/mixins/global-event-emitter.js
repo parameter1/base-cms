7968a013d8d3057de66c434d84c262c8e48a7b26
Josh Worden <josh@parameter1.com>
2022-08-10 15:47:29 -0500

    Revert "Revert event-emitter change"
    This reverts commit e7bb3a491fcb1e7aeccfb6efddbb0aa4620ca201.

--- packages/marko-web-identity-x/browser/mixins/global-event-emitter.js
+++ packages/marko-web-identity-x/browser/mixins/global-event-emitter.js
@@ -8,7 +8,13 @@ export default {
   },
   methods: {
     emit(name, data) {
-      const payload = { ...data, ...this.additionalEventData };
+      const payload = {
+        ...data,
+        ...this.additionalEventData,
+        // Ensure the `actionSource` is emitted with client-side events
+        ...(this.source && { actionSource: this.source }),
+        ...(this.loginSource && { actionSource: this.loginSource }),
+      };
       const { EventBus } = this;
       this.$emit(name, payload);
       EventBus.$emit(`identity-x-${name}`, payload);
