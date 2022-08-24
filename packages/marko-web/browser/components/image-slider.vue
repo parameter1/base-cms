e2988a339590c147f91b5071b8126a9038cc86ce
Shinsina <shinsina@att.net>
2022-08-24 08:48:05 -0500

    Add image-slider Vue component

---
+++ packages/marko-web/browser/components/image-slider.vue
@@ -0,0 +1,87 @@
+<template>
+  <div :class="classNames">
+    <ol class="carousel-indicators">
+      <li
+        v-for="(image, index) in images"
+        :key="index"
+        :class="{ active: index === activeIndex }"
+        @click="set(index)"
+      />
+    </ol>
+    <div class="carousel-inner">
+      <image-slide
+        v-for="(image, index) in images"
+        :key="index"
+        :length="images.length"
+        :index="index"
+        :image="image"
+        :active-index="activeIndex"
+      />
+    </div>
+    <a
+      href="#previous-slide"
+      class="carousel-control-prev"
+      role="button"
+      @click="decrement"
+    >
+      <span class="carousel-control-prev-icon" aria-label="Previous Slide" aria-hidden="true" />
+    </a>
+    <a
+      href="#next-slide"
+      class="carousel-control-next"
+      role="button"
+      @click="increment"
+    >
+      <span class="carousel-control-next-icon" aria-label="Next Slide" aria-hidden="true" />
+    </a>
+  </div>
+</template>
+
+<script>
+import ImageSlide from './image-slide.vue';
+
+export default {
+  components: {
+    ImageSlide,
+  },
+  props: {
+    images: {
+      type: Array,
+      required: true,
+    },
+    modifiers: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data: () => ({
+    activeIndex: 0,
+    blockName: 'carousel',
+  }),
+
+  computed: {
+    classNames() {
+      const { blockName } = this;
+      return [
+        blockName,
+        ...this.modifiers.map(mod => `${blockName}--${mod}`),
+      ];
+    },
+  },
+
+  methods: {
+    set(index) {
+      this.activeIndex = index;
+    },
+    increment() {
+      const { activeIndex } = this;
+      const { length } = this.images;
+      this.activeIndex = (activeIndex + 1 >= length) ? activeIndex : activeIndex + 1;
+    },
+    decrement() {
+      const { activeIndex } = this;
+      this.activeIndex = (activeIndex - 1 < 0) ? activeIndex : activeIndex - 1;
+    },
+  },
+};
+</script>
