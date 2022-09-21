6048b8f2cccebdb71fb815420938164f475a3f0e
Josh Worden <josh@parameter1.com>
2022-09-21 12:51:49 -0500

    Move user check/create to service, remove extras

--- packages/marko-web-auth0-identity-x/after-callback.js
+++ packages/marko-web-auth0-identity-x/after-callback.js
@@ -1,19 +1,7 @@
-const { get } = require('object-path');
 const { decode } = require('jsonwebtoken');
-const gql = require('graphql-tag');
 
 const { log } = process.env.NODE_ENV === 'development' ? console : { log: v => v };
 const error = (...msgs) => process.stdout.write(...msgs);
-
-const findUser = gql`
-query LoginCheckAppUser($email: String!) {
-  appUser(input: { email: $email }) {
-    id
-    email
-    verified
-  }
-}
-`;
 
 /**
  * Syncs Auth0 and IdentityX user states
@@ -23,33 +11,26 @@ query LoginCheckAppUser($email: String!) {
  * @param {Object} session
  * @returns Object the Auth0 user session object
  */
-module.exports = async (req, res, session, decoded) => {
+module.exports = async (req, _, session) => {
   // Only handle if Auth0 & IdentityX are loaded
   if (!req.identityX) throw new Error('IdentityX must be enabled and configured!');
 
   const { identityX: service } = req;
-  const { token, client } = service;
+  const { token } = service;
   const user = await decode(session.id_token);
 
   // If there's no Auth0 context, or an IdX context already exists, there's nothing to do here.
   if (!user || token) return session;
 
-  log('A0+IdX.cb', { email: user && user.email, token, decoded });
+  log('A0+IdX.cb', { email: user && user.email, token });
 
   // Destroy A0 context if no email is present
   const { email, email_verified: ev } = user;
   if (!email || !ev) throw new Error('Auth0 user must provide a verified email address.');
 
-  // Attempt to load the IdentityX AppUser by email address
-  const qr = await client.query({ query: findUser, variables: { email } });
-  let appUser = get(qr, 'data.appUser');
-  if (appUser) log('A0+IdX.cb', 'retrieved user', appUser && appUser.id);
-
-  // Create the IdentityX AppUser if they couldn't be retrieved
-  if (!appUser) {
-    appUser = await service.createAppUser({ email });
-    log('A0+IdX.cb', 'created user', appUser);
-  }
+  // Upsert the IdentityX AppUser
+  const appUser = await service.createAppUser({ email });
+  log('A0+IdX.cb', 'retrieved user', appUser && appUser.id);
 
   // federate trusted verification state to IdX and log in via impersonation api
   try {
