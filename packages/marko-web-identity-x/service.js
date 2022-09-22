10ab52029e336fdbeee9206b5bd12bfe3311f729
Josh Worden <josh@parameter1.com>
2022-09-22 13:14:39 -0500

    Add useIps and forceQuery flags to override ctx

--- packages/marko-web-identity-x/service.js
+++ packages/marko-web-identity-x/service.js
@@ -34,12 +34,12 @@ class IdentityX {
    *
    * @returns {Promise<object>}
    */
-  async loadActiveContext() {
+  async loadActiveContext({ forceQuery = false, useIps = false } = {}) {
     // Require a token/cookie to check active context. This disables team context from IP/CIDR.
-    if (!this.token) return {};
+    if (!this.token && !useIps) return {};
 
     // Only run the active context query once
-    if (!this.activeContextQuery) {
+    if (!this.activeContextQuery || forceQuery) {
       this.activeContextQuery = this.client.query({ query: getActiveContext });
     }
     const { data = {} } = await this.activeContextQuery;
