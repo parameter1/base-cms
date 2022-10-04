26f51f92e63518c818437cc9e4315065423e1bf8
Brandon Krigbaum <brandonbk@gmail.com>
2022-10-04 16:51:18 -0500

    Retain oly_enc_id cookie on logout

--- packages/marko-web-omeda-identity-x/integration-hooks/on-logout.js
+++ packages/marko-web-omeda-identity-x/integration-hooks/on-logout.js
@@ -1,12 +1,1 @@
-const Joi = require('@parameter1/joi');
-const { validate } = require('@parameter1/joi/utils');
-const olyticsCookie = require('@parameter1/base-cms-marko-web-omeda/olytics/customer-cookie');
-
-module.exports = (params = {}) => {
-  const {
-    res,
-  } = validate(Joi.object({
-    res: Joi.object().required(),
-  }).unknown(), params);
-  olyticsCookie.clearFrom(res);
-};
+module.exports = {};
