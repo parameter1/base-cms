af9fcde777f5c6d75d6a0573ed8f2648f2bbdbb8
Brandon Krigbaum <brandonbk@gmail.com>
2022-10-10 10:55:57 -0500

    Add delimiter support

--- packages/marko-web/components/node/utils/image-values.js
+++ packages/marko-web/components/node/utils/image-values.js
@@ -1,12 +1,17 @@
 const imageHeight = require('./image-height');
 
-module.exports = ({ fluid, ar, width = 320 }) => {
+module.exports = ({
+  fluid,
+  ar,
+  width = 320,
+  delimiter = ':',
+}) => {
   if (fluid === true) {
-    if (ar) return { modifier: `fluid-${ar.replace(':', 'by')}` };
+    if (ar) return { modifier: `fluid-${ar.replace(delimiter, 'by')}` };
     return { modifier: 'fluid' };
   }
   if (ar) {
-    return { width, height: imageHeight(width, ar) };
+    return { width, height: imageHeight(width, ar, delimiter) };
   }
   return { width };
 };
