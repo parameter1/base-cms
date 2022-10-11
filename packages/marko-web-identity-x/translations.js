2607ec7b8a176991d93e23631e6c1b0914d185aa
Shinsina <shinsina@att.net>
2022-10-11 12:24:07 -0500

    Add i18n function and translations list.

---
+++ packages/marko-web-identity-x/translations.js
@@ -0,0 +1,48 @@
+module.exports = {
+  en: {
+    // Step 2 Form Labels
+    firstNameLabel: 'First Name',
+    familyNameLabel: 'Last Name',
+    emailLabel: 'Email Address',
+    companyLabel: 'Company Name',
+    jobTitleLabel: 'Job Title',
+    countryLabel: 'Country',
+    countryPlaceholder: 'Select country...',
+    zipLabel: 'Postal/ZIP Code',
+    submitLabel: 'Submit',
+    // On Login Dialogue
+    almostDone: 'Almost Done!',
+    sentAnEmail: 'We just sent an email to',
+    oneTimeLoginLink: 'with your one-time login link',
+    toFinish: 'To finish',
+    actionText: 'logging in',
+    openEmail: 'open the email message and click the link within',
+    pleaseCheckLine1: 'Note: please check your spam/junk folders',
+    pleaseCheckLine2: 'If you do not receive this email, your firewall or ISP has likely blocked it.',
+    pleaseAdd: 'Please add',
+    whiteList: 'to your whitelist and try registing again',
+  },
+  es: {
+    // Step 2 Form Labels
+    firstNameLabel: 'Nombre',
+    familyNameLabel: 'Apellidos',
+    emailLabel: 'Correo electrónico',
+    companyLabel: 'Compañía',
+    jobTitleLabel: 'Nombre de tu cargo',
+    countryLabel: 'País',
+    countryPlaceholder: 'Selecciona un país...',
+    zipLabel: 'Código postal',
+    submitLabel: 'Enviar',
+    // On Login Dialogue
+    almostDone: '¡Falta Muy Poco!',
+    sentAnEmail: 'Enviamos un correo a',
+    oneTimeLoginLink: 'con su enlace de incio de sesión único.',
+    toFinish: 'Para terminar',
+    actionText: 'de registrarse para recibir sus notificaciones por correo electrónico',
+    openEmail: 'abra el mensaje de correo electrónico y haga clic en el enlace que contiene',
+    pleaseCheckLine1: 'Nota: por favor revise sus carpetas de spam/basura',
+    pleaseCheckLine2: 'Si no recibe este correo electrónico, es probable que su firewall o ISP lo haya bloqueado',
+    pleaseAdd: 'Agregue',
+    whitelist: ' a su lista blanca e intente registrarse nuevamente',
+  },
+};
