6485461f92eb0ea92f1902f4957509a2ae035a6a
Brian Miller <brian@parameter1.com>
2022-11-01 09:01:55 -0500

    Update how sideloadDataFn is implemented
    Check to see if this.sideloadDataFN is a function if it isnt set it to a simple function that returns an empty object.  Update calls after that to always set sideloadedData.

--- packages/marko-web/middleware/page-node/index.js
+++ packages/marko-web/middleware/page-node/index.js
@@ -1,3 +1,4 @@
+const { isFunction: isFn } = require('@parameter1/base-cms-utils');
 const { getAsObject } = require('@parameter1/base-cms-object-path');
 const ResolvedNode = require('./resolved');
 
@@ -24,19 +25,14 @@ class PageNode {
       const { queryFragment, variables, resultField } = this;
       const path = `data.${resultField}`;
       const query = this.queryFactory({ queryFragment, queryName: 'PageNode' });
-      if (typeof this.sideloadDataFn === 'function') {
-        this.sideloadDataFn.bind(this);
-        this.promise = Promise.all([
-          this.apolloClient.query({ query, variables }),
-          this.sideloadDataFn(),
-        ]).then(([r, sideloadedData]) => {
-          this.sideloadedData = sideloadedData;
-          return createNode(getAsObject(r, path));
-        });
-      } else {
-        this.promise = this.apolloClient.query({ query, variables })
-          .then(r => createNode(getAsObject(r, path)));
-      }
+      // Ensure there is a sideloadDataFn set, if not return empty object.
+      if (!isFn(this.sideloadDataFn)) this.sideloadDataFn = () => {};
+      const [r, sideloadedData] = await Promise.all([
+        this.apolloClient.query({ query, variables }),
+        this.sideloadDataFn(),
+      ]);
+      this.promise = createNode(getAsObject(r, path));
+      this.sideloadedData = sideloadedData;
     }
     return this.promise;
   }
