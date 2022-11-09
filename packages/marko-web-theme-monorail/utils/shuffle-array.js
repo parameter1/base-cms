868c5a6afd07077749af312fb4ab7dc834c744e3
Brandon Krigbaum <brandonbk@gmail.com>
2022-11-09 08:50:47 -0600

    Add shuffle-array utility

---
+++ packages/marko-web-theme-monorail/utils/shuffle-array.js
@@ -0,0 +1,9 @@
+module.exports = (a) => {
+  const shuffled = [...a];
+
+  for (let i = shuffled.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+};
