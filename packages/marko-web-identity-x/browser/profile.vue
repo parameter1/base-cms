6bdc6d8c164759b1d6e77cc2b8f6ffc7c2761299
Brian Miller <brian@parameter1.com>
2022-11-11 12:08:33 -0600

    update success message classes

--- packages/marko-web-identity-x/browser/profile.vue
+++ packages/marko-web-identity-x/browser/profile.vue
@@ -177,15 +177,15 @@
       </p>
     </form>
     <div v-else>
-      <div class="success-message">
-        <div class="success-message__title">
+      <div class="text-success">
+        <div class="text-success__title">
           Your profile has been saved.
         </div>
-        <div v-if="returnTo" class="success-message__message">
+        <div v-if="returnTo" class="text-success__message">
           If you are not automatically redirected <a :href="returnTo">click here</a> to continue.
         </div>
-        <div v-else class="success-message__message">
-          <a href="#" @click="handleReload()">Click here</a> to continue modiying your profile
+        <div v-else class="text-success__message">
+          <span class="btn-link" @click="handleReload()">Click here</span> to continue modiying your profile
           or <a href="/">click here</a> to return to the home page.
         </div>
       </div>
