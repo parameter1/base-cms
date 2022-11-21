e9502442186659164b68ce438e518fca875f976f
Josh Worden <josh@parameter1.com>
2022-11-21 11:01:10 -0600

    Make omeda optional in example website

--- services/example-website/index.js
+++ services/example-website/index.js
@@ -1,5 +1,7 @@
 const { startServer } = require('@parameter1/base-cms-marko-web');
 const omedaIdentityX = require('@parameter1/base-cms-marko-web-omeda-identity-x');
+const auth0IdentityX = require('@parameter1/base-cms-marko-web-auth0-identity-x');
+const basicIdentityX = require('@parameter1/base-cms-marko-web-identity-x');
 const { set, getAsObject } = require('@parameter1/base-cms-object-path');
 const contactUs = require('@parameter1/base-cms-marko-web-contact-us');
 const omedaNewsletters = require('@parameter1/base-cms-marko-web-omeda/routes/omeda-newsletters');
@@ -7,7 +9,6 @@ const newsletterState = require('@parameter1/base-cms-marko-web-theme-monorail/m
 const contentGating = require('@parameter1/base-cms-marko-web-theme-monorail/middleware/content-gating');
 const i18n = require('@parameter1/base-cms-marko-web-theme-monorail/middleware/i18n');
 const loadInquiry = require('@parameter1/base-cms-marko-web-inquiry');
-const auth0IdX = require('@parameter1/base-cms-marko-web-auth0-identity-x');
 const document = require('./server/components/document');
 const coreConfig = require('./config/core');
 const siteConfig = require('./config/site');
@@ -18,6 +19,7 @@ const recaptcha = require('./config/recaptcha');
 
 const { log } = console;
 const useAuth0 = Boolean(process.env.AUTH0_ENABLED === '1');
+const useOmeda = Boolean(process.env.OMEDA_ENABLED === '1');
 
 const routes = config => (app) => {
   // Handle submissions on /__inquiry
@@ -52,13 +54,16 @@ module.exports = startServer({
     if (useAuth0) {
       // Setup IdentityX + Auth0
       const config = getAsObject(siteConfig, 'auth0');
-      auth0IdX(app, { ...config, idxConfig, idxRouteTemplates });
+      auth0IdentityX(app, { ...config, idxConfig, idxRouteTemplates });
       log('Enabled IdentityX + Auth0');
-    } else {
+    } else if (useOmeda) {
       // Setup IdentityX + Omeda
       const oidxConfig = getAsObject(siteConfig, 'omedaIdentityX');
       omedaIdentityX(app, { ...oidxConfig, idxRouteTemplates });
       log('Enabled IdentityX + Omeda');
+    } else {
+      basicIdentityX(app, idxConfig, { templates: idxRouteTemplates });
+      log('Enabled IdentityX');
     }
   },
 }).then(() => log('Website started!')).catch(e => setImmediate(() => { throw e; }));
