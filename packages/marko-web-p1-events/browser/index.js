054e95c77314e0f5a67196ecf581dd96d8536df3
Josh Worden <josh@parameter1.com>
2022-11-22 09:37:09 -0600

    Push IdX ENL events to P1Events

--- packages/marko-web-p1-events/browser/index.js
+++ packages/marko-web-p1-events/browser/index.js
@@ -1,10 +1,28 @@
 const TrackContentBodyLinks = () => import(/* webpackChunkName: "p1-events-track-content-body-links" */ './track-content-body-links.vue');
 const TrackInquirySubmission = () => import(/* webpackChunkName: "p1-events-track-inquiry-submission" */ './track-inquiry-submission.vue');
 
+const track = (...args) => window.p1events || function p1events() {
+  (window.p1events.q = window.p1events.q || []).push(...args);
+};
+
 export default (Browser) => {
   const { EventBus } = Browser;
   Browser.register('P1EventsTrackContentBodyLinks', TrackContentBodyLinks);
   Browser.register('P1EventsTrackInquirySubmission', TrackInquirySubmission, {
     provide: { EventBus },
+  });
+
+  // Provide raw event tracking for integrated services
+  EventBus.$on('identity-x-newsletter-form-action', (props) => {
+    const { category, action, label } = props;
+    // Other standard props: ctx, entity, props
+    const { ctx, entity } = props;
+    track('track', {
+      category,
+      action,
+      label,
+      ctx,
+      entity,
+    });
   });
 };
