54dfd475ede5012462fc9b00baaada3c1fbbbfd0
Jake Collins <shinsina@att.net>
2022-12-01 09:34:04 -0600

    Remove package level Dayjs configuration

--- packages/marko-core/dayjs.js
+++
@@ -1,6 +0,0 @@
-const dayjs = require('dayjs');
-const advancedFormat = require('dayjs/plugin/advancedFormat');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
-
-module.exports = dayjs.extend(advancedFormat).extend(utc).extend(timezone);
