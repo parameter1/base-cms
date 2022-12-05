21e6f4d02e84ee940259ad1358dfec646e201797
Jake Collins <shinsina@att.net>
2022-12-05 11:04:42 -0600

    Require necessary locale on dayjs initialization

--- packages/dayjs/index.js
+++ packages/dayjs/index.js
@@ -2,5 +2,6 @@ const dayjs = require('dayjs');
 const advancedFormat = require('dayjs/plugin/advancedFormat');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
+require('dayjs/locale/es');
 
 module.exports = dayjs.extend(advancedFormat).extend(utc).extend(timezone);
