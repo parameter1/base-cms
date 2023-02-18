b5d82428d687a0cd0b542ccaa591f3357b1770df
Jacob Bare <jacob@parameter1.com>
2023-02-18 15:29:42 -0600

    Resolve listen and restart with website message

--- packages/marko-web-modern/cli/serve/fork-server.js
+++ packages/marko-web-modern/cli/serve/fork-server.js
@@ -26,23 +26,25 @@ module.exports = ({
   const listen = async ({ rejectOnNonZeroExit = true } = {}) => {
     await kill();
     proc = fork(file, [], { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] });
-    await new Promise((resolve, reject) => {
+    const msg = await new Promise((resolve, reject) => {
       proc.on('message', (message) => {
-        if (message.event === 'ready') resolve();
+        if (message.event === 'ready') resolve(message);
       });
       proc.on('exit', (code) => {
         if (code && code !== 0 && rejectOnNonZeroExit) reject(new Error(`Forked process received a non-zero (${code}) exit code.`));
       });
     });
     if (isFn(onReady)) await onReady();
+    return msg;
   };
 
   return {
     kill,
     listen,
     restart: async (...args) => {
-      await listen(...args);
+      const message = await listen(...args);
       if (isFn(onAfterRestart)) await onAfterRestart();
+      return message;
     },
   };
 };
