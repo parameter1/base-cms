11af55ce59cefeb242bf1d9e9738b1d074620bc9
Jacob Bare <jacob@parameter1.com>
2023-02-23 17:31:14 -0600

    Re-enable in-body error check and report messages

--- packages/marko-web/integration/test-website-boot.js
+++ packages/marko-web/integration/test-website-boot.js
@@ -1,27 +1,36 @@
 const fetch = require('node-fetch');
+const { htmlEntities } = require('@parameter1/base-cms-html');
 
 const { error, log } = console;
 
+const url = process.env.MARKO_WEB_INTEGRATION_TEST_URL || 'http://localhost:80';
+
 setInterval(async () => {
   try {
-    const res = await fetch('http://localhost:80', { method: 'get' });
+    const res = await fetch(url, { method: 'get' });
     if (!res.ok) {
       error('Response not ok!', res.status, res.statusText);
       process.exit(1);
     } else {
       const html = await res.text();
+      // check for in-body body errors
+      const matches = [...html.matchAll(/data-marko-error="(.*?)"/g)];
+      const errors = [];
+      matches.forEach((values) => {
+        const value = values[1];
+        errors.push(htmlEntities.decode(value));
+      });
+      if (errors.length) {
+        error('In-page, server-side Marko error(s) were encountered!', errors);
+        process.exit(0);
+      }
+
+      // if not in-body errors, ensure page rendered.
       const found = /.*<\/head>.*<\/body>.*<\/html>.*/is.test(html);
       if (!found) {
         error('Unable to find closing HTML tags!');
         process.exit(1);
-        return;
       }
-      // now check for any server errors
-      // if (/data-marko-error="true"/g.test(html)) {
-      //   error('An in-page server-side Marko error was encountered!');
-      //   process.exit(1);
-      //   return;
-      // }
       log('Integration tests passed!');
       process.exit(0);
     }
