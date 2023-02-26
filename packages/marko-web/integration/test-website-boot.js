a48dcf88ebcea27b27d690f91b34acfc17641ad5
Jacob Bare <jacob@parameter1.com>
2023-02-26 09:20:43 -0600

    Add readiness check and wait/check intervals

--- packages/marko-web/integration/test-website-boot.js
+++ packages/marko-web/integration/test-website-boot.js
@@ -1,40 +1,107 @@
-const fetch = require('node-fetch');
 const { htmlEntities } = require('@parameter1/base-cms-html');
+const { sleep: wait, cleanPath } = require('@parameter1/base-cms-utils');
+const whilst = require('async/whilst');
+const fetch = require('node-fetch');
 
-const { error, log } = console;
+const { log } = console;
 
-const url = process.env.MARKO_WEB_INTEGRATION_TEST_URL || 'http://localhost:80';
+const origin = process.env.MARKO_WEB_INTEGRATION_TEST_URL || 'http://localhost:80';
 
-setInterval(async () => {
+const fetchResponse = async ({
+  path = '/',
+  catchErrors = false,
+  returnNullWhenNotOk = false,
+} = {}) => {
+  const url = `${cleanPath(origin)}/${cleanPath(path)}`;
+  log(`fetching ${url}`);
+  const opts = { method: 'get' };
+  if (!catchErrors) return fetch(url, opts);
   try {
-    const res = await fetch(url, { method: 'get' });
-    if (!res.ok) {
-      error('Response not ok!', res.status, res.statusText);
-      process.exit(1);
-    } else {
-      const html = await res.text();
-      // check for in-body body errors
-      const matches = [...html.matchAll(/data-marko-error="(.*?)"/g)];
-      const errors = [];
-      matches.forEach((values) => {
-        const value = values[1];
-        errors.push(htmlEntities.decode(value));
-      });
-      if (errors.length) {
-        error('In-page, server-side Marko error(s) were encountered!', errors);
-        process.exit(1);
-      }
-
-      // if not in-body errors, ensure page rendered.
-      const found = /.*<\/head>.*<\/body>.*<\/html>.*/is.test(html);
-      if (!found) {
-        error('Unable to find closing HTML tags!');
-        process.exit(1);
-      }
-      log('Integration tests passed!');
-      process.exit(0);
-    }
+    const res = await fetch(url, opts);
+    if (returnNullWhenNotOk && !res.ok) return null;
+    return res;
   } catch (e) {
-    // noop
+    return null;
   }
-}, 100);
+};
+
+const checkReadiness = async ({
+  path = '/_health',
+  startAfter = 5000,
+  checkInterval = 1000,
+  unhealthAfter = 5,
+} = {}) => {
+  log('checking readiness...');
+  log(`initially waiting for ${startAfter}ms before checking...`);
+  await wait(startAfter);
+
+  let timesChecked = 0;
+  let booted = false;
+  await whilst(async () => !booted, async () => {
+    timesChecked += 1;
+    if (timesChecked > unhealthAfter) {
+      throw new Error('The readiness probe has reached its maximum check limit.');
+    }
+    log(`pinging health check, attempt number ${timesChecked}...`);
+    const res = await fetchResponse({ path, catchErrors: true });
+    // if response is null, a connection error occrred
+    if (res == null || (res && !res.ok)) {
+      if (res == null) log('did not receive a response - assuming not ready.');
+      if (res && !res.ok) log(`received a non-ok response from health check - ${res.status} ${res.statusText}`);
+      log(`waiting another ${checkInterval}ms before retrying.`);
+      await wait(checkInterval);
+      return;
+    }
+    booted = true;
+  });
+  log('container is ready.');
+};
+
+const testPage = async ({ path, retryAttempts = 3 } = {}) => {
+  log(`testing page path ${path}`);
+
+  let timesChecked = 0;
+  let passed = false;
+  await whilst(async () => !passed, async () => {
+    timesChecked += 1;
+    if (timesChecked > retryAttempts) {
+      throw new Error(`The test runner for page path ${path} has reached its maximum check limit.`);
+    }
+    const res = await fetchResponse({ path });
+    if (!res.ok) throw new Error(`Received a non-ok response from path page ${path}`, res.status, res.statusText);
+    const html = await res.text();
+
+    // first ensure the entire page rendered. if it didn't a fatal backened error occurred
+    // that prevented rendering.
+    const found = /.*<\/head>.*<\/body>.*<\/html>.*/is.test(html);
+    if (!found) throw new Error(`The page at path ${path} did not completely render.`);
+
+    // then check for in-body errors. this means an async internal block failed
+    // but the page could fully render.
+    const matches = [...html.matchAll(/data-marko-error="(.*?)"/g)];
+    const errors = [];
+    matches.forEach((values) => {
+      const value = values[1];
+      errors.push(htmlEntities.decode(value));
+    });
+    if (errors.length) {
+      // if all the errors were timeout errors, let's try again.
+      if (errors.every((msg) => msg === 'Timed out after 10000ms')) {
+        log(`all errors for page path ${path} were timeout errors. retrying...`);
+        return;
+      }
+      // otherwise error.
+      log({ errors });
+      throw new Error(`Encountered server-side Marko error(s) at page path ${path}`);
+    }
+    passed = true;
+  });
+  log(`page path ${path} passed tests.`);
+};
+
+const run = async () => {
+  await checkReadiness();
+  await testPage({ path: '/' });
+};
+
+run().catch((e) => setImmediate(() => { throw e; }));
