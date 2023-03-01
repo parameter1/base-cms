06bd6a22d0ce00cb1fe52f597dc3abbbf14a58ad
Jacob Bare <jacob@parameter1.com>
2023-03-01 12:12:46 -0600

    Remove purge logic from core marko; add entry template path

--- packages/marko-express/middleware/core.js
+++ packages/marko-express/middleware/core.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 process.env.MARKO_DEBUG = false;
 require('marko');
-const path = require('path');
 const buildGlobal = require('../utils/build-global');
 
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -10,29 +9,14 @@ const express = module.main ? module.main.require('express') : require('express'
 if (!express) throw new Error('Unable to load Express.');
 
 const patch = (response) => {
-  response.marko = response.marko || async function markoResponse(template, data) {
+  response.marko = response.marko || function markoResponse(template, data) {
     if (typeof template === 'string') throw new Error('res.marko does not accept a string or path as a template.');
 
     const res = this;
     const { req } = res;
     const $global = buildGlobal(res, data);
 
-    const shouldPurge = ['cookies', 'query'].some((key) => {
-      const { __purgecss: purge } = req[key];
-      return purge && !['false', '0', 'null'].includes(purge);
-    });
-    if (shouldPurge) {
-      const { config } = $global;
-      const params = new URLSearchParams({
-        // get the incoming, entry template file location
-        template: template.path.replace(/\.marko\.js$/, '.marko'),
-        // get the full css file
-        css: path.join(config.get('rootDir'), config.styles()[0]),
-      });
-      // set the stand-in route name to be used by the document stylesheet rel
-      $global.__purgecss = `/__purgecss?${params}`;
-    }
-
+    $global.entryTemplatePath = template.path.replace(/\.marko\.js$/, '.marko');
     res.set({ 'content-type': 'text/html; charset=utf-8' });
     return template.render({ ...(data || {}), $global }, res).on('error', req.next);
   };
