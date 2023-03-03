cd24e585cb5c32df0f0ed3ca81adbad3eec60762
Jacob Bare <jacob@parameter1.com>
2023-03-02 22:35:50 -0600

    Setup critical css builds in cli

--- packages/web-cli/build/css.js
+++ packages/web-cli/build/css.js
@@ -1,6 +1,7 @@
 const vite = require('vite');
 const path = require('path');
 const purgecss = require('@fullhuman/postcss-purgecss');
+const critical = require('postcss-critical-split');
 const handleWatch = require('./utils/handle-watch');
 
 const purgeGlobs = ['**/*.marko', '**/*.vue'];
@@ -10,53 +11,59 @@ module.exports = async ({
   entry,
   watch = false,
   onFileChange,
-  purge = false,
   purgeContentDirs = [],
 } = {}) => {
-  const common = {
-    appType: 'custom',
-    mode: 'production',
-    clearScreen: false,
-    build: {
-      rollupOptions: { input: path.resolve(cwd, entry) },
-      outDir: './dist/css/',
-      manifest: true,
-      sourcemap: true,
-      watch: watch ? {} : false,
-    },
+  const file = path.resolve(cwd, entry);
+
+  /**
+   *
+   * @param {object} params
+   * @param {string} params.dir
+   * @param {import("vite").CSSOptions} params.css
+   * @returns {import("vite").InlineConfig}
+   */
+  const options = ({ dir, css }) => {
+    /** @type {import("vite").InlineConfig} */
+    const opts = {
+      appType: 'custom',
+      mode: 'production',
+      clearScreen: false,
+      build: {
+        rollupOptions: { input: file },
+        assetsDir: '.',
+        outDir: `./dist/css/${dir}`,
+        manifest: true,
+        watch: watch ? {} : false,
+      },
+      css,
+    };
+    return opts;
   };
 
+  const purge = purgecss({
+    content: purgeGlobs.reduce((arr, glob) => {
+      [cwd, ...purgeContentDirs].forEach((dir) => {
+        arr.push(path.resolve(cwd, path.join(dir, glob)));
+      });
+      return arr;
+    }, []),
+    safelist: {
+      standard: [/--/, /__/],
+    },
+  });
+
   const maybeWatchers = await Promise.all([
-    vite.build(common),
-    ...purge ? [
-      vite.build({
-        ...common,
-        build: {
-          ...common.build,
-          outDir: './dist/purgedcss/',
-        },
-        css: {
-          postcss: {
-            plugins: [
-              purgecss({
-                content: purgeGlobs.reduce((arr, glob) => {
-                  [cwd, ...purgeContentDirs].forEach((dir) => {
-                    arr.push(path.resolve(cwd, path.join(dir, glob)));
-                  });
-                  return arr;
-                }, []),
-                safelist: {
-                  standard: [/--/, /__/],
-                },
-              }),
-            ],
-          },
-        },
-      }),
-    ] : [],
+    vite.build(options({
+      dir: 'critical',
+      css: { postcss: { plugins: [purge, critical({ output: 'critical' })] } },
+    })),
+    vite.build(options({
+      dir: 'main',
+      css: { postcss: { plugins: [purge, critical({ output: 'rest' })] } },
+    })),
   ]);
 
-  await Promise.all(maybeWatchers.map(async (maybeWatcher) => handleWatch({
+  await Promise.all(maybeWatchers.map((maybeWatcher) => handleWatch({
     watch,
     maybeWatcher,
     onFileChange,
