5f220c70ffa67c6d5ca46508efeadc91845937fa
Jacob Bare <jacob@parameter1.com>
2023-03-03 15:59:04 -0600

    Register `blocks/most-popular-list` with render block loader
    Closes #622

--- packages/marko-web-theme-monorail/routes/render-block.js
+++ packages/marko-web-theme-monorail/routes/render-block.js
@@ -1,10 +1,11 @@
-const { asyncRoute } = require('@parameter1/base-cms-utils');
 const createError = require('http-errors');
 const MostPopular = require('../components/blocks/most-popular');
+const MostPopularList = require('../components/blocks/most-popular-list');
 const TopStoriesMenu = require('../components/blocks/top-stories-menu');
 
 const blocks = {
   'most-popular': MostPopular,
+  'most-popular-list': MostPopularList,
   'top-stories-menu': TopStoriesMenu,
 };
 
@@ -17,11 +18,15 @@ const parseJSON = (value) => {
 };
 
 module.exports = (app) => {
-  app.get('/__render-block/:name', asyncRoute(async (req, res) => {
+  app.get('/__render-block/:name', (req, res) => {
     const { name } = req.params;
     const input = parseJSON(req.query.input);
     const Component = blocks[name];
     if (!Component) throw createError(404, `No block component found for '${name}'`);
     return res.marko(Component, input);
-  }));
+  }, (err, req, res, next) => { // eslint-disable-line
+    res.set({ 'content-type': 'text/html; charset=utf-8' });
+    res.status(err.status || err.statusCode || 500);
+    res.send(`Unable to render marko component. ${err.message}`);
+  });
 };
