7495ddaf03b12e647141353c46bbbe785493cfa7
Jacob Bare <jacob@parameter1.com>
2023-03-09 14:57:18 -0600

    Rework to support critical modules and custom minification
    Since the default vite minification was breaking comments, cssnano is used at the end of the css build process and vite’s minifcation is disable.
    
    Critical modules were also enabled, e.g. `/*! critical:start|content */`

--- packages/web-cli/build/css.js
+++ packages/web-cli/build/css.js
@@ -1,26 +1,22 @@
-const vite = require('vite');
 const path = require('path');
-const purgecss = require('@fullhuman/postcss-purgecss');
-const critical = require('postcss-critical-split');
-const { writeFile, mkdir, rm } = require('fs').promises;
+const { rm } = require('fs').promises;
 const del = require('del');
 const { dim, cyan } = require('chalk');
 const {
+  buildCriticals,
+  buildMain,
   buildManifest,
-  buildOptions,
-  formatOutput,
+  buildPurged,
+  getCSSFromOutput,
   logBuildStep,
+  minify,
+  write,
 } = require('./utils/css');
-
-const purgeGlobs = ['**/*.marko', '**/*.vue'];
 
 const { log } = console;
 
 /**
- * @typedef {import("./utils/css.js").CSSOutputTarget} CSSOutputTarget
- * @typedef {import("./utils/css.js").FormattedOutputAsset} FormattedOutputAsset
- * @typedef {import("./utils/css.js").OutputAsset} OutputAsset
- * @typedef {import("postcss").AcceptedPlugin} AcceptedPlugin
+ * @typedef {import("./utils/css.js").RollupWatcher} RollupWatcher
  *
  * @param {object} params
  * @param {string} params.cwd
@@ -41,94 +37,33 @@ module.exports = async ({
   /**
    *
    * @param {object} params
-   * @param {CSSOutputTarget} params.target
-   * @param {CSSOutputTarget} [params.from]
-   * @param {string} params.fileName
-   * @param {AcceptedPlugin[]} [params.plugins]
-   * @param {boolean} [params.embedded]
-   * @returns {Promise<FormattedOutputAsset>}
+   * @param {CSSOutputAsset} params.main
    */
-  const build = async ({
-    target,
-    from,
-    fileName,
-    plugins,
-    embedded,
-  }) => {
-    logBuildStep(target);
-    const file = path.resolve(cwd, from ? `${dir}/${fileName}` : fileName);
-    const result = await vite.build(buildOptions({
-      input: file,
-      css: { postcss: { plugins } },
-      watch: false,
-    }));
-
-    /** @type {OutputAsset} */
-    const output = result.output[0];
-    const formatted = await formatOutput(output, { target, embedded });
-
-    const targetDir = path.resolve(cwd, dir);
-    await mkdir(targetDir, { recursive: true });
-    await writeFile(path.resolve(targetDir, formatted.file), output.source, 'utf8');
-    return formatted;
-  };
-
-  const buildDependents = async ({ file }) => {
-    const purged = await build({
-      target: 'purged',
-      from: 'all',
-      fileName: file,
-      plugins: [
-        purgecss({
-          content: purgeGlobs.reduce((arr, glob) => {
-            [cwd, ...purgeContentDirs].forEach((d) => {
-              arr.push(path.resolve(cwd, path.join(d, glob)));
-            });
-            return arr;
-          }, []),
-          safelist: { standard: [/--/, /__/] },
-        }),
-      ],
-    });
-
-    const criticals = await Promise.all([
-      build({
-        target: 'critical',
-        from: 'purged',
-        fileName: purged.file,
-        plugins: [critical({ output: 'critical' })],
-        embedded: true,
-      }),
-      build({
-        target: 'optimized',
-        from: 'purged',
-        fileName: purged.file,
-        plugins: [critical({ output: 'rest' })],
-      }),
-    ]);
-
-    return [purged, ...criticals];
+  const build = async ({ main }) => {
+    const purged = await buildPurged({ cwd, source: main.source, contentDirs: purgeContentDirs });
+    const criticals = await buildCriticals({ source: purged.source });
+    const minified = await minify({ assets: [main, purged, ...criticals] });
+    const written = await write({ cwd, dir, assets: minified });
+    await buildManifest({ cwd, dir, written });
+    return written;
   };
 
   if (!inWatchMode) {
     // normal build
+    logBuildStep();
 
     // clean directory
     await rm(path.resolve(cwd, dir), { recursive: true, force: true });
 
-    // build main css + all dependent css versions
-    const all = await build({ target: 'all', fileName: entry });
-    const dependents = await buildDependents({ file: all.file });
-    await buildManifest({ dir, assets: [all, ...dependents] });
+    /** @type {CSSOutputAsset} */
+    const main = await buildMain({ cwd, entry, watch: false });
+    await build({ main });
     return;
   }
 
   // watch mode
-  /** @type {import("rollup").RollupWatcher} */
-  const watcher = await vite.build(buildOptions({
-    input: path.resolve(cwd, entry),
-    watch: true,
-  }));
+  /** @type {RollupWatcher} */
+  const watcher = await buildMain({ cwd, entry, watch: true });
 
   // clear vite watchers so we can manually control dependent builds and writes
   watcher.removeAllListeners();
@@ -138,23 +73,18 @@ module.exports = async ({
   });
   watcher.on('event', async (event) => {
     const { code } = event;
-    const target = 'all';
-    if (code === 'BUNDLE_START') logBuildStep(target);
+    if (code === 'BUNDLE_START') logBuildStep();
     if (code === 'BUNDLE_END') {
       await (async () => {
-        // write the file to the dist folder.
-        const { output } = await event.result.write({
+        const { output } = await event.result.generate({
           dir: path.resolve(cwd, dir),
-          assetFileNames: 'all-[hash][extname]',
         });
-        const all = await formatOutput(output[0], { target });
 
-        // build dependents and manifest
-        const dependents = await buildDependents({ file: all.file });
-        await buildManifest({ dir, assets: [all, ...dependents] });
+        const main = getCSSFromOutput(output);
+        const written = await build({ main });
 
         // clean old files
-        const pathsToDelete = [all, ...dependents].map(({ file }) => `!${path.resolve(dir, file)}`);
+        const pathsToDelete = written.map(({ file }) => `!${path.resolve(dir, file)}`);
         await del([path.resolve(dir, './*.css'), ...pathsToDelete]);
       })().then(() => {
         event.result.close();
--- packages/web-cli/build/utils/css.js
+++ packages/web-cli/build/utils/css.js
@@ -1,7 +1,13 @@
 const vite = require('vite');
 const path = require('path');
-const { writeFile } = require('fs').promises;
+const postcss = require('postcss');
+const { writeFile, mkdir } = require('fs').promises;
+const purgecss = require('@fullhuman/postcss-purgecss');
+const critical = require('postcss-critical-split');
+const cssnano = require('cssnano');
+const defaultPreset = require('cssnano-preset-default');
 const { gzip } = require('zlib');
+const crypto = require('crypto');
 const {
   bold,
   cyan,
@@ -12,14 +18,26 @@ const formatFileSize = require('./format-file-size');
 
 const { log } = console;
 
+const purgeGlobs = ['**/*.marko', '**/*.vue'];
+const preset = defaultPreset({ discardComments: { removeAll: true } });
+
+const createHash = (source) => {
+  const hash = crypto.createHash('sha256').update(source).digest('hex');
+  return hash.substring(0, 8);
+};
+
 /**
- * @typedef {('all'|'purged'|'critical'|'optimized')} CSSOutputTarget
  * @typedef {import("rollup").OutputAsset} OutputAsset
- * @typedef {import("vite").CSSOptions} CSSOptions
- * @typedef {import("vite").InlineConfig} InlineConfig
+ * @typedef {import("rollup").RollupOutput} RollupOutput
+ * @typedef {import("rollup").RollupWatcher} RollupWatcher
  *
- * @typedef FormattedOutputAsset
- * @prop {CSSOutputTarget} target The file output target type, e.g. all or critical
+ * @typedef CSSOutputAsset
+ * @prop {string} key
+ * @prop {string} source
+ * @prop {boolean} [embedded]
+ *
+ * @typedef WrittenCSSOutputAsset
+ * @prop {string} key The file output target key, e.g. index, purged or critical
  * @prop {string} file The name of the written file, include it's content hash
  * @prop {number} size The file size, in bytes
  * @prop {number} compressed The gzipped file size, in bytes.
@@ -27,43 +45,179 @@ const { log } = console;
  */
 
 /**
- * @param {object} params
- * @param {string} params.input The absolute input file.
- * @param {CSSOptions} [params.css] Additional CSS build options.
- * @param {boolean} params.watch Whether to put the vite/rollup build into watch mode.
- * @returns {InlineConfig}
+ *
+ * @param {OutputAsset[]} output
+ * @returns {CSSOutputAsset}
  */
-const buildOptions = ({ input, watch, css }) => ({
-  appType: 'custom',
-  mode: 'production',
-  clearScreen: false,
-  logLevel: 'warn',
-  // do not minify, otherwise critical and purge comments aren't properly passed through.
-  esbuild: { minifySyntax: false },
-  build: {
-    rollupOptions: { input },
-    assetsDir: '.',
-    write: false,
-    watch: watch ? { skipWrite: true } : null,
-  },
-  css,
-});
+const getCSSFromOutput = (output) => {
+  const chunk = output[0];
+  const { name, source } = chunk;
+  const key = path.basename(name, path.extname(name));
+  return { key, source, embedded: false };
+};
 
 /**
-   * @param {object} params
-   * @param {string} params.dir The CSS dist directory
-   * @param {FormattedOutputAsset[]} params.assets The asset outputs
-   * @returns {Promise<object>}
-   */
-const buildManifest = async ({ dir, assets }) => {
+ *
+ * @param {object} params
+ * @param {string} params.cwd The current working directory.
+ * @param {string} params.entry The entry SASS file
+ * @param {boolean} [params.watch]
+ * @returns {Promise<CSSOutputAsset|RollupWatcher>}
+ */
+const buildMain = async ({ cwd, entry, watch }) => {
+  const file = path.resolve(cwd, entry);
+  const result = await vite.build({
+    appType: 'custom',
+    mode: 'production',
+    clearScreen: false,
+    logLevel: 'warn',
+    build: {
+      minify: false,
+      rollupOptions: { input: file },
+      assetsDir: '.',
+      write: false,
+      watch: watch ? { skipWrite: true } : null,
+    },
+  });
+  if (watch) return result;
+  const { output } = result;
+  return getCSSFromOutput(output);
+};
+
+/**
+ *
+ * @param {object} params
+ * @param {string} params.cwd The current working directory
+ * @param {string} params.source The CSS source to purge
+ * @param {string[]} params.contentDirs Additional directories to extract content from.
+ * @returns {Promise<CSSOutputAsset>}
+ */
+const buildPurged = async ({ cwd, source, contentDirs = [] }) => {
+  /** @type {import("postcss").Result} */
+  const result = await postcss([
+    purgecss({
+      content: purgeGlobs.reduce((arr, glob) => {
+        [cwd, ...contentDirs].forEach((d) => {
+          arr.push(path.resolve(cwd, path.join(d, glob)));
+        });
+        return arr;
+      }, []),
+      safelist: { standard: [/--/, /__/] },
+    }),
+  ]).process(source, { from: undefined });
+  return { key: 'purged', source: result.css, embedded: false };
+};
+
+/**
+ *
+ * @param {object} params
+ * @param {string} params.source
+ * @returns {Promise<CSSOutputAsset[]>}
+ */
+const buildCriticals = async ({ source }) => {
+  // extract the critical modules names that are in-use (if any)
+  /** @type {string[]} */
+  const modules = [...(source.match(/\/\*!\s*?critical.*?\s*?\*\//g) || []).reduce((set, match) => {
+    if (/:end/.test(match)) return set;
+    const matches = /\|(.+[^\s*?])\s*?\*\//.exec(match);
+    if (matches && matches[1]) set.add(matches[1]);
+    return set;
+  }, new Set())];
+
+  const keys = ['critical', 'optimized'];
+  const settings = [];
+  keys.forEach((key) => {
+    const opts = {
+      key,
+      embedded: key === 'critical',
+      output: key === 'critical' ? key : 'rest',
+    };
+    settings.push(opts);
+    modules.forEach((name) => {
+      settings.push({
+        ...opts,
+        key: `${key}-${name}`,
+        modules: [name],
+        separator: '|',
+      });
+    });
+  });
+
+  return Promise.all(settings.map(async ({ key, embedded, ...options }) => {
+    /** @type {import("postcss").Result} */
+    const result = await postcss([critical(options)]).process(source, { from: undefined });
+    return { key, source: result.css, embedded };
+  }));
+};
+
+/**
+ *
+ * @param {object} params
+ * @param {CSSOutputAsset[]} params.assets
+ * @returns {Promise<CSSOutputAsset[]>}
+ */
+const minify = async ({ assets }) => Promise.all(assets.map(async (asset) => {
+  /** @type {import("postcss").Result} */
+  const result = await postcss([cssnano({ preset })]).process(asset.source, { from: undefined });
+  return { ...asset, source: result.css };
+}));
+
+/**
+ *
+ * @param {object} params
+ * @param {string} params.cwd
+ * @param {string} params.dir
+ * @param {CSSOutputAsset[]} params.assets
+ * @returns {Promise<WrittenCSSOutputAsset[]>}
+ */
+const write = async ({ cwd, dir, assets }) => {
+  // only write assets that have code. its possible some optimized assets can be empty.
+  const eligible = assets.filter((asset) => asset.source);
+
+  // make the dir
+  await mkdir(path.resolve(cwd, dir), { recursive: true });
+
+  return Promise.all(eligible.map(async (asset) => {
+    const { source, ...rest } = asset;
+    const hash = createHash(source);
+    const file = `${asset.key}-${hash}.css`;
+
+    const compressed = await new Promise((resolve, reject) => {
+      gzip(source, (err, r) => {
+        if (err) return reject(err);
+        return resolve(r);
+      });
+    });
+
+    const absolute = path.resolve(cwd, dir, file);
+    await writeFile(absolute, source, 'utf8');
+
+    return {
+      ...rest,
+      embedded: Boolean(asset.embedded),
+      file,
+      size: source.length,
+      compressed: compressed.length,
+    };
+  }));
+};
+
+/**
+ * @param {object} params
+ * @param {string} params.cwd The current working directory
+ * @param {string} params.dir The CSS dist directory
+ * @param {WrittenCSSOutputAsset[]} params.written The written assets
+ * @returns {Promise<object>}
+ */
+const buildManifest = async ({ cwd, dir, written }) => {
   const manifest = {};
   const longest = {
     file: 0,
     size: 0,
     compressed: 0,
   };
-  assets.forEach(({ target, ...rest }) => {
-    manifest[target] = rest;
+  written.forEach(({ key, ...rest }) => {
+    manifest[key] = rest;
     const { file } = rest;
     const loc = path.join(dir, file);
     if (loc.length > longest.file) longest.file = loc.length;
@@ -89,47 +243,23 @@ const buildManifest = async ({ dir, assets }) => {
     log(str);
   });
 
-  await writeFile(path.resolve(dir, 'manifest.json'), JSON.stringify(manifest, null, 2), 'utf8');
+  await writeFile(path.resolve(cwd, dir, 'manifest.json'), JSON.stringify(manifest, null, 2), 'utf8');
   return manifest;
-};
-
-/**
- * @param {OutputAsset} output
- * @param {object} params
- * @param {CSSOutputTarget} params.target The file output target type, e.g. all or critical
- * @param {boolean} [params.embedded] Whether the output should be flagged as embeddable
- * @returns {Promise<FormattedOutputAsset>}
- */
-const formatOutput = async (output, { target, embedded }) => {
-  const { name, source } = output;
-  const basename = path.basename(name, path.extname(name));
-  const targetFileName = output.fileName.replace(basename, target);
-
-  const compressed = await new Promise((resolve, reject) => {
-    gzip(source, (err, r) => {
-      if (err) return reject(err);
-      return resolve(r);
-    });
-  });
-
-  return {
-    target,
-    file: targetFileName,
-    size: source.length,
-    compressed: compressed.length,
-    embedded: Boolean(embedded),
-  };
 };
 
 /**
  *
  * @param {CSSOutputTarget} target
  */
-const logBuildStep = (target) => log(cyan(`vite v${vite.version}`), green(`building ${target} css for production...`));
+const logBuildStep = () => log(cyan(`vite v${vite.version}`), green('building css for production...'));
 
 module.exports = {
+  buildCriticals,
+  buildMain,
   buildManifest,
-  buildOptions,
-  formatOutput,
+  buildPurged,
+  getCSSFromOutput,
   logBuildStep,
+  minify,
+  write,
 };
