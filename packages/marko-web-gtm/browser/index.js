394731340cb99143b301b78e1490c067f87e76f8
Brian Miller <brian@parameter1.com>
2023-04-19 13:22:58 -0500

    Move DataLayer EventBus hook from gtm to identityX package
    Move the call to add the EventBuss.$on event listener to push stuff to the data layer out of the gtm package and into the identityX pacakge

--- packages/marko-web-gtm/browser/index.js
+++ packages/marko-web-gtm/browser/index.js
@@ -11,47 +11,4 @@ export default (Browser) => {
   Browser.register('GTMTrackBusEvent', GTMTrackBusEvent, {
     provide: { EventBus },
   });
-
-  // Send emitted IdentityX events to the datalayer
-  window.dataLayer = window.dataLayer || [];
-  [
-    // Views
-    'identity-x-authenticate-mounted',
-    'identity-x-comment-stream-mounted',
-    'identity-x-comment-post-mounted',
-    'identity-x-comment-create-mounted',
-    'identity-x-login-mounted',
-    'identity-x-logout-mounted',
-    'identity-x-profile-mounted',
-    // Actions/submissions
-    'identity-x-authenticated',
-    'identity-x-auto-signup',
-    'identity-x-comment-post-submitted',
-    'identity-x-comment-report-submitted',
-    'identity-x-comment-stream-loaded',
-    'identity-x-comment-stream-loaded-more',
-    'identity-x-login-link-sent',
-    'identity-x-logout',
-    'identity-x-profile-updated',
-    // Errors
-    'identity-x-authenticate-errored',
-    'identity-x-comment-post-errored',
-    'identity-x-comment-report-errored',
-    'identity-x-comment-stream-errored',
-    'identity-x-login-errored',
-    'identity-x-logout-errored',
-    'identity-x-profile-errored',
-  ].forEach((event) => {
-    EventBus.$on(event, (args) => {
-      if (!window.IdentityX) return;
-      window.dataLayer.push({
-        event,
-        'identity-x': {
-          ...(args && args),
-          event,
-          loginSource: window.IdentityX.getLoginSource(),
-        },
-      });
-    });
-  });
 };
