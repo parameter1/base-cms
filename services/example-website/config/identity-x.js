9a9e7ce410c95c006a0eb4105e07ec7e97cde23a
Josh Worden <josh@parameter1.com>
2023-04-25 17:17:12 -0500

    Adds support for identifying active custom fields

--- services/example-website/config/identity-x.js
+++ services/example-website/config/identity-x.js
@@ -4,6 +4,23 @@ const { log } = console;
 
 module.exports = new IdentityXConfiguration({
   appId: process.env.IDENTITYX_APP_ID,
+  ...(process.env.IDENTITYX_CONTEXT === 'parameter1.io' ? {
+    // Parameter1.io
+    appContextId: '64483ba86ffc39e3e8cf13c4',
+    activeCustomFieldIds: [
+      '618a8a62934f8400ad4beb8f', // Favorite food
+      '626fe8854e597205b368a50f', // Favorite color
+      '62c4883255955d8d69e1b4ca', // DMN magazine
+    ],
+  } : {
+    // Parameter1.dev
+    appContextId: '64483b96361977163db5e286',
+    activeCustomFieldIds: [
+      '618ab74dcd3e2f0147386c42', // Favorite games
+      '626fe91c79d99b27544a4c9f', // Pina coladas
+      '628e98dc2da307ff2ef6a8b0', // Rain
+    ],
+  }),
   apiToken: process.env.IDENTITYX_API_TOKEN,
   requiredServerFields: ['givenName', 'familyName', 'countryCode'],
   requiredClientFields: ['regionCode', 'countryCode'],
