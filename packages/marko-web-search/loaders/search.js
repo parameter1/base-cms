ffcdcc4f90ba3432358b689f2c83177ac1e10c03
Jake Collins <shinsina@att.net>
2023-06-07 10:48:08 -0500

    useMatchPhrase -> searchType, simplify conditional

--- packages/marko-web-search/loaders/search.js
+++ packages/marko-web-search/loaders/search.js
@@ -64,7 +64,7 @@ module.exports = async ({ apolloBaseCMS, apolloBaseBrowse } = {}, {
 
   queryFragment,
   opSuffix,
-  useMatchPhrase = false,
+  searchType,
 } = {}) => {
   if (!apolloBaseCMS || !apolloBaseBrowse) throw new Error('Both the BaseCMS and Base Browse Apollo clients must be provided.');
   const input = {
@@ -72,8 +72,7 @@ module.exports = async ({ apolloBaseCMS, apolloBaseBrowse } = {}, {
     statuses: ['PUBLISHED'],
     contentTypes,
     countryCodes,
-    ...(!useMatchPhrase && searchQuery && { search: { query: searchQuery } }),
-    ...(useMatchPhrase && searchQuery && { search: { query: searchQuery, type: 'MATCH_PHRASE' } }),
+    ...(searchQuery && { search: { query: searchQuery, type: searchType } }),
     ...((assignedToWebsiteSiteIds.length || assignedToWebsiteSectionIds.length) && {
       assignedToWebsites: {
         ...(assignedToWebsiteSiteIds.length && { siteIds: assignedToWebsiteSiteIds }),
