f17578b0d48d28209f81f872156d459857fd5220
Josh Worden <josh@parameter1.com>
2023-06-15 13:38:33 -0500

    Create download route to store submission

---
+++ packages/marko-web-identity-x/routes/download.js
@@ -0,0 +1,22 @@
+const gql = require('graphql-tag');
+const { asyncRoute } = require('@parameter1/base-cms-utils');
+
+const mutation = gql`
+  mutation StoreContentDownloadSubmission($input: CreateContentDownloadSubmissionMutationInput!) {
+    createContentDownloadSubmission(input:$input) {
+      id
+    }
+  }
+`;
+
+module.exports = asyncRoute(async (req, res) => {
+  const { body, apollo } = req;
+  const { contentId, payload } = body;
+  const input = {
+    contentId,
+    payload,
+    ipAddress: req.ip,
+  };
+  await apollo.mutate({ mutation, variables: { input } });
+  res.json({ ok: true });
+});
