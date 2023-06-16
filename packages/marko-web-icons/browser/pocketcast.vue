175a960bd213cf8eeaeb734a307a56e0e4ba489c
Brian Miller <brian@parameter1.com>
2023-06-16 11:19:56 -0500

    Add Pocketcast icon set

---
+++ packages/marko-web-icons/browser/pocketcast.vue
@@ -0,0 +1,38 @@
+<template>
+  <icon-wrapper
+    name="pocketcast"
+    :tag="tag"
+    :block-name="blockName"
+    :class-name="className"
+    :modifiers="modifiers"
+  >
+    <!-- eslint-disable-next-line -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12,0C5.372,0,0,5.372,0,12c0,6.628,5.372,12,12,12c6.628,0,12-5.372,12-12 C24,5.372,18.628,0,12,0z M15.564,12c0-1.968-1.596-3.564-3.564-3.564c-1.968,0-3.564,1.595-3.564,3.564 c0,1.968,1.595,3.564,3.564,3.564V17.6c-3.093,0-5.6-2.507-5.6-5.6c0-3.093,2.507-5.6,5.6-5.6c3.093,0,5.6,2.507,5.6,5.6H15.564z M19,12c0-3.866-3.134-7-7-7c-3.866,0-7,3.134-7,7c0,3.866,3.134,7,7,7v2.333c-5.155,0-9.333-4.179-9.333-9.333 c0-5.155,4.179-9.333,9.333-9.333c5.155,0,9.333,4.179,9.333,9.333H19z"/></svg>
+  </icon-wrapper>
+</template>
+
+<script>
+import IconWrapper from './_wrapper.vue';
+
+export default {
+  components: { IconWrapper },
+  props: {
+    blockName: {
+      type: String,
+      default: 'marko-web-icon',
+    },
+    className: {
+      type: String,
+      default: null,
+    },
+    modifiers: {
+      type: Array,
+      default: () => ([]),
+    },
+    tag: {
+      type: String,
+      default: 'span',
+    },
+  },
+};
+</script>
