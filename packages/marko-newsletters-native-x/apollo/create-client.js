cc3e676d4d44c48da4e239b97f453de194130347
Brian Miller <brian@parameter1.com>
2023-08-10 12:18:13 -0500

    Update newsletters nativeX package to use similar config setup as web package
    Add nativeX configuration for native x pacakge.  will utilize similar calls to web package.  Like getUri() isEnabled() and set get placements calls

---
+++ packages/marko-newsletters-native-x/apollo/create-client.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch');
+
+const getOperationName = (string) => {
+  const matches = /query\s+([a-z0-9]+)[(]?.+{/gi.exec(string);
+  if (matches && matches[1]) return matches[1];
+  return undefined;
+};
+
+module.exports = (uri) => Object.create({
+  query: async ({ query, variables, headers }) => {
+    const body = JSON.stringify({
+      operationName: getOperationName(query),
+      variables,
+      query,
+    });
+
+    const res = await fetch(uri, {
+      method: 'POST',
+      headers: {
+        ...headers,
+        'content-type': 'application/json',
+      },
+      body,
+    });
+    const json = await res.json();
+    if (!res.ok || (json && json.errors)) {
+      if (!json || !json.errors) {
+        const err = new Error(`An unknown, fatal GraphQL error was encountered (${res.status})`);
+        err.statusCode = res.status;
+        throw err;
+      }
+      const [networkError] = json.errors;
+      const err = new Error(networkError.message);
+      const { extensions } = networkError;
+      if (extensions) err.code = extensions.code;
+      if (extensions && extensions.exception) err.statusCode = extensions.exception.statusCode;
+      throw err;
+    }
+    return json;
+  },
+});
