176ed0103bef98b6c4cd99664edc9090b247b964
Jacob Bare <jacob@parameter1.com>
2023-08-16 14:22:04 -0500

    Consistently handle browser component script on server

--- packages/marko-web/utils/render-ssr-component-from-marko.js
+++ packages/marko-web/utils/render-ssr-component-from-marko.js
@@ -1,9 +1,19 @@
 const renderComponent = require('./render-ssr-component');
+const BrowserComponentScript = require('../components/browser-component/script');
 
-module.exports = ({ id, input, out }) => {
-  const { name } = input;
+module.exports = async ({ id, input, out }) => {
+  const { name, props } = input;
   const { compiledVueComponents } = out.global;
   const Component = compiledVueComponents[name];
   if (!Component) throw new Error(`No compiled Vue template found for ${name}. Unable to render server-side.`);
-  return renderComponent(Component, { id, props: input.props });
+  const html = await renderComponent(Component, { id, props });
+  const script = BrowserComponentScript.renderToString({
+    id,
+    hydrate: true,
+    name,
+    props,
+    skipWhenExists: input.skipWhenExists,
+  });
+  console.log({ script });
+  return `${html}${script}`;
 };
