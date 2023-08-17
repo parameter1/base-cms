613ea90601df2e7ceb77feb6bd721baede5fd17d
Brian Miller <brian@parameter1.com>
2023-08-17 09:06:59 -0500

    Add inline idxNewsletterSignup component support for inbody injection
    as well as update current monorail component with new utilz for setting & getting props

--- packages/marko-web-theme-monorail/browser/ssr.js
+++ packages/marko-web-theme-monorail/browser/ssr.js
@@ -2,12 +2,14 @@ import ThemeMenuToggleButton from './menu-toggle-button.vue';
 import ThemeNewsletterToggleButton from './newsletter-toggle-button.vue';
 import IdentityXNewsletterToggleButton from './idx-newsletter-form/toggle-button.vue';
 import IdentityXNewsletterFormPushdown from './idx-newsletter-form/pushdown.vue';
+import IdentityXNewsletterFormInline from './idx-newsletter-form/inline.vue';
 import ThemeSiteNewsletterMenu from './site-newsletter-menu.vue';
 
 export default {
   ThemeMenuToggleButton,
   IdentityXNewsletterToggleButton,
   IdentityXNewsletterFormPushdown,
+  IdentityXNewsletterFormInline,
   ThemeNewsletterToggleButton,
   ThemeSiteNewsletterMenu,
 };
