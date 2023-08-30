017f285c0a3452fccfb8509df7bec86b549f4642
Jessica Wade <jessica.wade@parameter1.com>
2023-08-30 08:05:56 -0500

    Add Twitter X icon

---
+++ packages/marko-web-icons/browser/twitter-x.vue
@@ -0,0 +1,38 @@
+<template>
+  <icon-wrapper
+    name="twitter"
+    :tag="tag"
+    :block-name="blockName"
+    :class-name="className"
+    :modifiers="modifiers"
+  >
+    <!-- eslint-disable-next-line -->
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter X icon</title><path xmlns="http://www.w3.org/2000/svg" d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>
+  </icon-wrapper>
+</template>
+
+<script>
+import IconWrapper from './_wrapper.vue';
+
+export default {
+  components: { IconWrapper },
+  props: {
+    blockName: {
+      type: String,
+      default: 'marko-web-icon',
+    },
+    className: {
+      type: String,
+      default: null,
+    },
+    modifiers: {
+      type: Array,
+      default: () => ([]),
+    },
+    tag: {
+      type: String,
+      default: 'span',
+    },
+  },
+};
+</script>
