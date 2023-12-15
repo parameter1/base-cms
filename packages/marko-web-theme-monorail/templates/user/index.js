e8fdc9f515addf4ab6bfae2b2b09066c7ba8eea3
Brian Miller <brian@parameter1.com>
2023-12-15 09:14:46 -0600

    Move global monorail user templates out of site org repos
    Move out of org repos into shared theme-monorail repo

---
+++ packages/marko-web-theme-monorail/templates/user/index.js
@@ -0,0 +1,13 @@
+const authenticate = require('./authenticate');
+const login = require('./login');
+const logout = require('./logout');
+const register = require('./register');
+const profile = require('./profile');
+
+module.exports = {
+  authenticate,
+  login,
+  logout,
+  register,
+  profile,
+};
