4c7b5b5855d0c29218493694d82f1f2c868e78cc
Shinsina <shinsina@att.net>
2023-12-27 12:26:51 -0600

    Utilize exceptions to "load" content regardless of status

--- packages/marko-web/utils/build-content-input.js
+++ packages/marko-web/utils/build-content-input.js
@@ -1,6 +1,9 @@
-module.exports = ({ req }) => {
+module.exports = ({ req, contentIdStatusExceptions }) => {
   const input = {};
-  if (req.cookies['preview-mode'] || req.query['preview-mode']) {
+  if (req.cookies['preview-mode']
+    || req.query['preview-mode']
+    || contentIdStatusExceptions.includes(Number(req.params.id))
+  ) {
     input.status = 'any';
   } else {
     input.since = Date.now();
