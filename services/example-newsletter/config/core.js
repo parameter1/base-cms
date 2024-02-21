e8dd0c8117a3541a44bc5061f876cfe5bbe33991
Josh Worden <josh@parameter1.com>
2024-02-21 14:04:49 -0600

    Create example newsletter app

---
+++ services/example-newsletter/config/core.js
@@ -0,0 +1,9 @@
+const config = {
+  daily: {
+    name: 'Sandbox Daily',
+    primaryColor: '#1f4391',
+    logo: { src: '/files/base/p1/sandbox/image/static/sandbox-logo.png?&auto=format,compress' },
+  },
+};
+
+module.exports = config;
