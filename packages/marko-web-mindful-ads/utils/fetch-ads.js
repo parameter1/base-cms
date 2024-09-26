2514e09e221e79dbdfcf29aa880d5152031b3b01
Josh Worden <josh@parameter1.com>
2024-09-26 10:49:25 -0500

    Support sending advertiserIds to mindful ads

--- packages/marko-web-mindful-ads/utils/fetch-ads.js
+++ packages/marko-web-mindful-ads/utils/fetch-ads.js
@@ -10,6 +10,7 @@ const host = process.env.MINDFUL_ADVERTISING_DELIVERY_API_URL || 'https://delive
  */
 module.exports = async ({
   adUnitId,
+  advertiserIds = [],
   debug = true,
   limit = 1,
   sizes = [],
@@ -20,6 +21,7 @@ module.exports = async ({
   const log = logger(debug);
   log({
     adUnitId,
+    advertiserIds,
     limit,
     sizes,
     namespace,
@@ -28,6 +30,7 @@ module.exports = async ({
     const url = new URL(`${host}/${namespace}/website-banner/${adUnitId}`);
     url.searchParams.set('limit', limit);
     sizes.forEach((size) => url.searchParams.append('sizes', size));
+    advertiserIds.forEach((id) => url.searchParams.append('advertiserIds', id));
     const res = await fetch(url);
     if (!res.ok) throw new Error(`Unable to retrieve mindful ads: ${res.status} ${res.statusText}`);
     return res.json();
@@ -40,6 +43,7 @@ module.exports = async ({
 /**
  * @typedef MindfulWebsiteBannerRequestArgs
  * @prop {string} namespace The Mindful namespace (tenant/workspace keys)
+ * @prop {string[]} advertiserIds
  * @prop {string} adUnitId
  * @prop {boolean} debug
  * @prop {string[]} sizes
