ea94b8c7cac5ae4e13553d71effc20dd97f60309
Jacob Bare <jacob@parameter1.com>
2024-10-04 15:52:09 -0500

    Add `Query.isUserAuthTokenValid`

--- services/graphql-server/src/graphql/resolvers/auth/user.js
+++ services/graphql-server/src/graphql/resolvers/auth/user.js
@@ -1,6 +1,21 @@
 module.exports = {
   Query: {
     activeUser: (_, __, { auth }) => auth.getUser(),
+
+    /**
+     * @param {void} _
+     * @param {object} args
+     * @param {string} args.value
+     * @param {import("../../../routes/graphql").GraphQLServerContext} contextValue
+     */
+    async isUserAuthTokenValid(_, { value }, { userService }) {
+      try {
+        await userService.checkAuth(value);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    },
   },
   Mutation: {
     login: (_, { input }, { userService }) => {
