2c89f992a0920dcbc6ef97101b1778bb64f06816
Brian Miller <brian@parameter1.com>
2024-10-10 10:09:22 -0500

    Rename to content-access-state to better match

--- packages/marko-web-identity-x/middleware/content-access-state.js
+++ packages/marko-web-identity-x/middleware/content-access-state.js
@@ -1,17 +1,17 @@
 const { get, getAsObject } = require('@parameter1/base-cms-object-path');
 
 const cookieNamePrefix = '__idx_form';
-const days = 1;
+const days = 0; // make them submit everytime for now untile we decide to chenge functionality.
 
-const updateContentIdxFormState = ({ res, content }) => {
-  // Handle setting of contentIdxFormState Object
+const contentAccessState = ({ res, content }) => {
+  // Handle setting of contentAccessState Object
   const { surveyType, surveyId } = getAsObject(content, 'gating');
   const cookieName = `${cookieNamePrefix}_${surveyId}_${content.id}`;
 
   const setFormDisplay = ({ req }) => {
-    res.locals.contentIdxFormState.displayForm = !get(req, `cookies.${cookieName}`);
+    res.locals.contentAccessState.displayForm = !get(req, `cookies.${cookieName}`);
   };
-  res.locals.contentIdxFormState = {
+  res.locals.contentAccessState = {
     displayForm: false,
     setFormDisplay,
   };
@@ -19,11 +19,11 @@ const updateContentIdxFormState = ({ res, content }) => {
   if (surveyType === 'idx') {
     const maxAge = days * 24 * 60 * 60 * 1000;
     const cookie = { name: cookieName, maxAge };
-    res.locals.contentIdxFormState.formId = surveyId;
-    res.locals.contentIdxFormState.cookie = cookie;
+    res.locals.contentAccessState.formId = surveyId;
+    res.locals.contentAccessState.cookie = cookie;
   }
 };
 
 module.exports = {
-  updateContentIdxFormState,
+  contentAccessState,
 };
