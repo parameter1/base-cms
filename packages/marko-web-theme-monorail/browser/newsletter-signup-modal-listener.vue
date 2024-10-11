7ecf1984d2b5faed3692ef45b4df1ca713a64b99
Brian Miller <brian@parameter1.com>
2024-10-10 20:26:57 -0500

    Account for target value of 0 and no cookie
    Allow for trigger on cookieValueToMatch of 0 so it triggers on initial page load.

--- packages/marko-web-theme-monorail/browser/newsletter-signup-modal-listener.vue
+++ packages/marko-web-theme-monorail/browser/newsletter-signup-modal-listener.vue
@@ -29,14 +29,18 @@ export default {
       const newsletterSignupModalElement = document.getElementById('newsletter-signup-modal');
       if (!window.location.pathname.match(/^\/user|\/page/)) {
         if (
-          this.hasCookie
-          && Number(this.cookieValue) === Number(this.cookieValueToMatch)
-          && window.dataLayer.find((dataEvent) => dataEvent['identity-x']
-          && dataEvent['identity-x'].newsletterSignupType === 'modal')
+          (!this.hasCookie && Number(this.cookieValueToMatch) === 0)
+          || (
+            this.hasCookie
+            && Number(this.cookieValue) === Number(this.cookieValueToMatch)
+            && window.dataLayer.find((dataEvent) => dataEvent['identity-x']
+            && dataEvent['identity-x'].newsletterSignupType === 'modal')
+          )
         ) {
           newsletterSignupModalElement.classList.add('newsletter-signup-modal-fade-in');
+          const value = !this.hasCookie ? 0 : (Number(this.cookieValue) + 1);
           // Set cookie, expires is in days so we have to divide by 24 for hours
-          cookies.set(this.cookieName, (Number(this.cookieValue) + 1), { expires: 1 / 24 });
+          cookies.set(this.cookieName, value, { expires: 1 / 24 });
         } else if (
           this.hasCookie
           && Number(this.cookieValue) < Number(this.cookieValueToMatch)
