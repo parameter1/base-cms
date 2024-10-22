ba3e6606170b00d656b347aa6b28b7a21d88d66d
Shinsina <46794001+Shinsina@users.noreply.github.com>
2024-10-22 10:50:17 -0500

    Correct linting errors

--- packages/marko-web-gtm/context/content.js
+++ packages/marko-web-gtm/context/content.js
@@ -28,7 +28,7 @@ module.exports = ({ obj, req }) => {
     name: s.name,
   }));
   const emailSchedules = getAsArray(content, 'emailSchedules')
-    .map((schedule) => ({ name: schedule.newsletter.name, deploymentDate: schedule.deploymentDate}))
+    .map((schedule) => ({ name: schedule.newsletter.name, deploymentDate: schedule.deploymentDate }))
     .sort((a, b) => a.deploymentDate - b.deploymentDate);
   const magazineSchedules = getAsArray(content, 'magazineSchedules')
     .map(({ issue: i }) => ({ name: i.name, mailDate: i.mailDate }))
@@ -66,6 +66,6 @@ module.exports = ({ obj, req }) => {
     authors,
     schedules,
     emailSchedules,
-    magazineSchedules
+    magazineSchedules,
   };
 };
