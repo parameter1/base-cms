503c7ed39d11fedfba0ecdb5aee6185d0b9d26fe
Shinsina <shinsina@att.net>
2024-10-24 11:00:55 -0500

    Hoist Vue translations to base-cms-leaders-program, update import

---
+++ packages/leaders-program/src/utils/translations-vue.js
@@ -0,0 +1,20 @@
+export default {
+  en: {
+    visitSiteLabel: 'Visit Site',
+    viewProfileLabel: 'View Profile',
+    featuredVideosLabel: 'Featured Videos',
+    featuredProductsLabel: 'Featured Products',
+    'view more': 'View more',
+    videos: 'videos',
+    products: 'products',
+  },
+  es: {
+    visitSiteLabel: 'Visite el sitio web',
+    viewProfileLabel: 'Vea el perfil',
+    featuredVideosLabel: 'Videos destacados',
+    featuredProductsLabel: 'Productos destacados',
+    'view more': 'Vea más',
+    videos: 'videos',
+    products: 'productos',
+  },
+};
