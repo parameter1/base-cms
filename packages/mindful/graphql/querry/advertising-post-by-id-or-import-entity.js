8fccd20506bf39f1323c9b0980515d844bd88f8b
Brian Miller <brian@parameter1.com>
2024-11-04 11:10:31 -0600

    call and spread shared post by id fragment

--- packages/mindful/graphql/querry/advertising-post-by-id-or-import-entity.js
+++ packages/mindful/graphql/querry/advertising-post-by-id-or-import-entity.js
@@ -1,57 +1,10 @@
 const gql = require('graphql-tag');
+const defaultFragment = require('../fragments/advertising-post-by-id');
 
 module.exports = gql`
 query advertisingPostByIdOrImportEntity($_id: ObjectID!, $provider: string, $tenant: string, $types: string) {
 advertisingPostByIdOrImportEntity(_id: $_id, provider: $provider, teanant: $tenant, type: $types) {
-  _id
-  title {
-    default
-  }
-  teaser
-  body
-  url
-  publishedDay
-  statusEdge {
-    node {
-      _id
-      label
-    }
-  }
-  featuredImageEdge {
-    _id
-    node {
-      _id
-      src {
-        url
-        settings {
-          fpX
-          fpY
-        }
-      }
-    }
-  }
-  companyEdge {
-    _id
-    node {
-      _id
-      name {
-        default
-      }
-      logoEdge {
-        _id
-        node {
-          _id
-          src {
-            url
-            settings {
-              fpX
-              fpY
-            }
-          }
-        }
-      }
-    }
-  }
+  ...${defaultFragment}
 }
 }
 `;
